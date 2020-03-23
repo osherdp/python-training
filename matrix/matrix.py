2bbe956f0d78d642b454b87421adef60ee2c6382
Yonatan Evantal <yonatan.evantal@gmail.com>
2020-03-23 18:36:52 +0200

    Stable version without documentation

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,11 +1,12 @@
 from itertools import repeat
+import operator
 
 
 class Matrix:
     """An object representing a matrix data-type."""
     __slots__ = 'matrix'
 
-    def __init__(self, *lines: tuple):
+    def __init__(self, *lines):
         """Constructor.
 
         lines: a list of tuples containing the data about the matrix.
@@ -25,11 +26,12 @@ class Matrix:
         contains tuples of the same length (Matrix only supports n * n) and
         their value is an integer."""
         # check if each line is a tuple.
-        if not all([type(line) == tuple for line in data]):
+        if not all([type(line_data) == tuple for line_data in data]):
             return False
 
         # check if each item in each line is an int.
-        if not all([type(item) == int for line in data for item in line]):
+        if not all([any([isinstance(item, int), isinstance(item, float)]) for
+                    line_data in data for item in line_data]):
             return False
 
         # checks if the given data is of a n * n matrix.
@@ -66,26 +68,86 @@ class Matrix:
         """Returns the matrix instance as a tuple of tuples."""
         return self.matrix
 
+    def _operation_on_matrix(self, other, operation):
+        if isinstance(other, Matrix):
+
+            if len(self.tuples) != len(other.tuples):
+                raise ValueError("Matrices length don't match")
+
+            matrix_data = [tuple
+                           ([operation(self.tuples[i][j], other.tuples[i][j])
+                             for j in range(len(self.tuples))]) for i in
+                           range(len(self.tuples))]
+
+            if operation == operator.eq:
+                return all(
+                    [item for line_data in matrix_data for item in line_data])
+
+            elif operation == operator.ne:
+                return any(
+                    [item for line_data in matrix_data for item in line_data])
+
+            return Matrix(*matrix_data)
+
+        elif isinstance(other, int) or isinstance(other, float):
+            return Matrix(
+                *[tuple([operation(self.tuples[i][j], other) for j in range(
+                    len(self.tuples))]) for i in range(len(self.tuples))])
+
     def __add__(self, other):
-        pass
+        """Adds two matrices."""
+        return self._operation_on_matrix(other, operator.mul)
 
     def __sub__(self, other):
-        pass
+        return self._operation_on_matrix(other, operator.sub)
 
     def __mul__(self, other):
-        pass
+        return self._operation_on_matrix(other, operator.mul)
 
-    def __divmod__(self, other):
-        pass
+    def __rmul__(self, other):
+        return self.__mul__(other)
 
+    def __truediv__(self, other):
+        return self._operation_on_matrix(other, operator.truediv)
 
-m = Matrix((1, 2), (3, 4))
-# m.matrix = ((1, 2), (3, 4))
-print(m)
-print(m.tuples, type(m.tuples), type(m.tuples[0]))
+    def __floordiv__(self, other):
+        return self.__truediv__(other)
 
-m = Matrix.unity(4)
-print(m)
+    def __eq__(self, other):
+        return self._operation_on_matrix(other, operator.eq)
+
+    def __ne__(self, other):
+        return self._operation_on_matrix(other, operator.ne)
+
+    def __iter__(self):
+        return iter(self.tuples)
+
+    def __hash__(self):
+        return hash(self.tuples)
+
 
 m = Matrix.ones(3)
 print(m)
+
+m2 = Matrix.unity(3)
+print(m2)
+
+print(m + m2)
+print(m2 - m)
+
+m = Matrix((1, 2), (3, 4))
+
+print(m * 5)
+print(10 * m)
+
+print(m / 2.5)
+
+m2 = Matrix.ones(2)
+print(m == m2)
+print(m != m2)
+
+for line in m:
+    print(line)
+
+dictionary = {Matrix((1, 1), (2, 2)): 2, Matrix((1, 1), (2, 3)): 3}
+print(dictionary)
