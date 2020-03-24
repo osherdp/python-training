94252cf342f3973def3b942d9c3fe29942467bb2
Yonatan Evantal <yonatan.evantal@gmail.com>
2020-03-24 23:17:25 +0200

    Fixes After Second CR

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,160 +1,157 @@
 import operator
-from itertools import repeat
+from collections.abc import Iterable
 
 
 class Matrix:
-    """An immutable object representing a matrix data-type. This
-    implementation only supports even-side matrices (n * n).
+    """An immutable object representing a matrix data-type.
 
     Attributes:
         _matrix (tuple): contains the data about the matrix. Each line of
-                         the matrix is represented by a tuple.
+            the matrix is represented by a tuple.
+    Note:
+        This implementation only supports even-side matrices (n * n).
     """
-    __slots__ = '_matrix'
 
     def __init__(self, *lines):
         self._validate(*lines)
-        super(Matrix, self).__setattr__('_matrix', tuple(lines))
+        self._matrix = tuple(lines)
 
-    def __setattr__(self, key, value):
-        """Defines that Matrix attributes are immutable."""
-        raise AttributeError(f'{self.__class__} instance cannot be modified')
+    def _validate(self, *data: tuple):
+        """Check if the data given is valid.
 
-    @staticmethod
-    def _validate(*data: tuple):
-        """Checks if the data given is valid. Valid data is defined if it
-        contains tuples of the same length (Matrix only supports n * n) and
-        their value is an integer."""
-        # check if each line is a tuple.
-        if not all([isinstance(line_data, tuple) for line_data in data]):
+        Note:
+            Valid data is defined if it contains tuples of the same length
+            (Matrix only supports n * n), items can't be iterable.
+        """
+        if not all(isinstance(line_data, tuple) for line_data in data):
             raise ValueError("Matrix lines must be tuple")
 
-        # check if each item in each line is an int or float.
-        if not all([any([isinstance(item, int), isinstance(item, float)]) for
-                    line_data in data for item in line_data]):
-            raise ValueError("Matrix items must be int or float")
+        if any(isinstance(item, Iterable) for line_data in data for item in
+               line_data):
+            raise ValueError("Matrix lines must be tuple")
 
-        # checks if the given data is of a n * n matrix.
         if not all([len(line) == len(data) for line in data]):
             raise ValueError("Matrix must be even-sided")
 
     @classmethod
-    def unity(cls, number_lines: int):
+    def unity(cls, dimension: int):
         """Generates a matrix with a main diagonal of 1's, everything else
         is 0's.
         """
-        tuples_in_chars = [
-            list(str('1' + ('0' * i)).zfill(number_lines)[::-1]) for i in
-            range(number_lines)]
-
-        return cls(*tuple(
-            [tuple(map(int, tuple_chars)) for tuple_chars in tuples_in_chars]))
+        return cls(*[tuple((0,) * j + (1,) + (0,) * i) for j, i in enumerate(
+            range(dimension - 1, -1, -1))])
 
     @classmethod
-    def ones(cls, number_lines):
+    def ones(cls, dimension):
         """Generates a matrix full of 1's."""
-        return cls(*tuple(
-            [tuple(repeat(1, number_lines)) for _ in range(number_lines)]))
+        return cls(*([tuple((1,) * dimension) for _ in range(dimension)]))
 
     def __repr__(self):
+        return super(Matrix, self).__repr__()
+
+    def __str__(self):
         """Returns a string presentation of the matrix instance."""
-        return f"({', '.join([str(line) for line in self._matrix])})"
+        return repr(self._matrix)
 
     @property
     def tuples(self):
         """Returns the matrix instance as a tuple of tuples."""
         return self._matrix
 
-    @staticmethod
-    def _bool_operation(operation, data) -> bool:
-        """Checks boolean operation output.
+    def _by_matrix(self, operation, dimension_of_other,
+                   self_item, other_item):
+        """Calculates operation on two items.
 
         Args:
-            operation (function): the boolean function to operate.
-            data (list): the data of the matrix (list of tuples).
+            operation (function): the function of the operator.
+            dimension_of_other (int): the number of lines in the other matrix.
+            self_item (number): current item from the current matrix instance.
+            other_item (number): current item from the other matrix instance.
 
         Returns:
-            If the operation is 'equal', returns true if all items are equal
-            to their parallel items in other, else false. If operation is
-            'not equal', return true if one or more items are not equal to
-            their parallel item/s in other, else false.
-        """
-        func = {operator.eq: all, operator.ne: any}.get(operation)
-        return func([item for line_data in data for item in line_data])
+            number, the result of the operation.
 
-    def _calculate_matrix_on_matrix(self, other, operation):
-        """Does mathematical operation on two matrices.
+        Note:
+            Boolean operations supports different dimensions matrices but
+            mathematical operations don't (except matrix division).
+        """
+
+        if dimension_of_other != len(self.tuples) and not any(
+                [operation == operator.eq, operation == operator.ne]):
+            raise ValueError("Matrices length don't match")
+
+        if operation == operator.truediv:
+            raise ValueError(f"{operation} not supported on "
+                             f"{self.__class__.__name__}")
+
+        return operation(self_item, other_item)
+
+    def _by_scalar(self, operation, item, scalar):
+        """Calculates operation on item by scalar.
 
         Args:
-            other (Matrix): the second matrix to do the operation on.
-            operation (function): the mathematical function to operate.
+            operation (function): the function of the operator.
+            item (number): current item from the current matrix instance.
+            scalar (number): the scalar to operate by.
 
         Returns:
-            List of tuples which contains the calculated matrix data.
-        """
-        return [tuple([operation(self.tuples[i][j], other.tuples[i][j]) for j
-                       in range(len(self.tuples))]) for i in range(len(
-                        self.tuples))]
+            number, the result of the operation.
 
-    def _calculate_scalar_on_matrix(self, other, operation):
-        """Does mathematical operation on scalar and matrix.
+        Note:
+            Scalar must be a number. Operations with scalars are only * and /.
+        """
+        if is_number(scalar) and not any([operation == operator.mul,
+                                          operation == operator.truediv]):
+            raise ValueError(f'{operation} not supported on {type(scalar)}')
+
+        return operation(item, scalar)
+
+    def _calculate_matrix_operation(self, other, operation):
+        """Does all the operations that can be done on a matrix.
 
         Args:
-            other (int or float): the scalar.
-            operation (function): the mathematical function to operate.
-
-        Returns:
-            List of tuples which contains the calculated matrix data.
-        """
-        return [tuple([operation(self.tuples[i][j], other) for j in range(
-            len(self.tuples))]) for i in range(len(self.tuples))]
-
-    def _operations_on_matrix(self, other, operation):
-        """Does all the operation that can be done on a matrix.
-        addition, subtraction, multiplication and division(on matrix or by
-        scalar). Also supports boolean operators == and !=.
-
-        Args:
-            other (Matrix or int or float): the other object to do the
-                                            operation.
+            other (Matrix or number): the other object to do the operation.
             operation (function): the function of the desired operator.
 
         Returns:
             If boolean operation, returns true if operation returned true
             else false. If mathematical operation, returns a new matrix with
             the calculated data.
+
+        Note:
+            Supported operations are addition, subtraction, multiplication and
+            division(on matrix or by scalar). Also supports boolean
+            operators == and !=.
         """
-        if isinstance(other, Matrix):
+        calculated_matrix = []
 
-            if len(self.tuples) != len(other.tuples) and not any(
-                    [operation == operator.eq, operation == operator.ne]):
-                raise ValueError("Matrices length don't match")
+        for i in range(len(self.tuples)):
 
-            matrix_data = self._calculate_matrix_on_matrix(other, operation)
+            calculated_line = []
+            for j in range(len(self.tuples)):
+                try:
+                    calculated_line.append(
+                        self._by_matrix(operation, len(other.tuples),
+                                        self.tuples[i][j], other.tuples[i][j]))
+                except AttributeError:
+                    calculated_line.append(
+                        self._by_scalar(operation, self.tuples[i][j], other))
 
-            if operation == operator.eq or operation == operator.ne:
-                return self._bool_operation(operation, matrix_data)
+            calculated_matrix.append(tuple(calculated_line))
 
-            return Matrix(*matrix_data)
-
-        elif any([isinstance(other, int) or isinstance(other, float)]) and any(
-                [operation == operator.mul, operation == operator.truediv]):
-            return Matrix(*self._calculate_scalar_on_matrix(other, operation))
-
-        else:
-            raise ValueError(f"{operation} not supported on {type(other)}")
+        return Matrix(*calculated_matrix)
 
     def __add__(self, other):
         """Adds two matrices."""
-        return self._operations_on_matrix(other, operator.add)
+        return self._calculate_matrix_operation(other, operator.add)
 
     def __sub__(self, other):
         """Subtract two matrices."""
-        return self._operations_on_matrix(other, operator.sub)
+        return self._calculate_matrix_operation(other, operator.sub)
 
     def __mul__(self, other):
         """Multiply two matrices or scalar by matrix."""
-        return self._operations_on_matrix(other, operator.mul)
+        return self._calculate_matrix_operation(other, operator.mul)
 
     def __rmul__(self, other):
         """Defines to handle reverse multiplication (10 * matrix) as regular
@@ -164,21 +161,28 @@ class Matrix:
     def __truediv__(self, other):
         """Divides two matrices or scalar by matrix. Returns floats if
         needed."""
-        return self._operations_on_matrix(other, operator.truediv)
+        return self._calculate_matrix_operation(other, operator.truediv)
 
     def __floordiv__(self, other):
         """Defines to handle floor division (5 / 2 = 2) as regular
         division."""
         return self.__truediv__(other)
 
+    def _check_bool_matrix(self, matrix, operation):
+        """Returns true if all the items in the matrix are true else false."""
+        func = {operator.eq: all, operator.ne: any}.get(operation)
+        return func([item for line_data in matrix.tuples for item in line_data])
+
     def __eq__(self, other):
         """Compares two matrices. Returns true if they are equal else false."""
-        return self._operations_on_matrix(other, operator.eq)
+        return self._check_bool_matrix(self._calculate_matrix_operation(
+            other, operator.eq), operator.eq)
 
     def __ne__(self, other):
         """Compares two matrices. Returns true if they are not equal else
         false."""
-        return self._operations_on_matrix(other, operator.ne)
+        return self._check_bool_matrix(self._calculate_matrix_operation(
+            other, operator.ne), operator.ne)
 
     def __iter__(self):
         """Enables to iterate on a matrix lines'."""
@@ -187,3 +191,11 @@ class Matrix:
     def __hash__(self):
         """Enables to use a matrix as key of a dictionary."""
         return hash(self.tuples)
+
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
--- matrix/tests_matrix.py
+++ matrix/tests_matrix.py
@@ -1,15 +1,16 @@
 import unittest
 
+import pytest
 from matrix import Matrix
 
 
+@pytest.mark.usefixtures("Matrix")
 class TestMatrix(unittest.TestCase):
+    """Unit tests for Matrix class."""
+
     def test_initialization(self):
         Matrix((1, 2), (3, 4))
         Matrix((1, 2), (5.5, 4))
-
-        with self.assertRaises(ValueError):
-            Matrix((1, 'a'), (3, 4))
 
         with self.assertRaises(ValueError):
             Matrix((1, 2), (3, [4, 5, 6]))
@@ -20,21 +21,20 @@ class TestMatrix(unittest.TestCase):
     def test_modification(self):
         m = Matrix((1, 1), (2, 2))
 
-        with self.assertRaises(AttributeError):
-            m._matrix = ((1, 2), (3, 4))
-
-        with self.assertRaises(AttributeError):
-            m._matrix = "hello world!"
+        with self.assertRaises(TypeError):
+            m._matrix[0] = (1, 2)
 
     def test_str(self):
-        m = Matrix((1, 1, 1), (2, 2, 2), (3, 3, 3))
+        data = (1, 1, 1), (2, 2, 2), (3, 3, 3)
+        m = Matrix(*data)
 
-        self.assertEqual(str(m), str(((1, 1, 1), (2, 2, 2), (3, 3, 3))))
+        self.assertEqual(str(m), str(data))
 
     def test_tuples(self):
-        m = Matrix((1, 1), (2, 2))
+        data = (1, 1), (2, 2)
+        m = Matrix(*data)
 
-        self.assertEqual(m.tuples, ((1, 1), (2, 2)))
+        self.assertEqual(m.tuples, data)
 
     def test_unity(self):
         self.assertEqual(
@@ -52,22 +52,20 @@ class TestMatrix(unittest.TestCase):
 
     def test_add(self):
         m1 = Matrix.ones(3)
-        m2 = Matrix.ones(3)
+        m2 = Matrix((11, 12, 13), (14, 15, 16), (17, 18, 19))
         m3 = Matrix.ones(2)
 
-        self.assertEqual(m1 + m2, Matrix((2, 2, 2), (2, 2, 2), (2, 2, 2)))
+        self.assertEqual(m1 + m2,
+                         Matrix((12, 13, 14), (15, 16, 17), (18, 19, 20)))
 
         with self.assertRaises(ValueError):
-            _ = m1 + m3
+            m1 + m3
 
         with self.assertRaises(ValueError):
-            _ = m1 + 10
+            m1 + 10
 
         with self.assertRaises(ValueError):
-            _ = m1 + 0.5
-
-        with self.assertRaises(ValueError):
-            _ = m1 - "0.5"
+            m1 + 0.5
 
     def test_sub(self):
         m1 = Matrix((1, 2), (3, 4))
@@ -77,16 +75,13 @@ class TestMatrix(unittest.TestCase):
         self.assertEqual(m1 - m2, Matrix((-4, -4), (-4, -4)))
 
         with self.assertRaises(ValueError):
-            _ = m1 - m3
+            m1 - m3
 
         with self.assertRaises(ValueError):
-            _ = m1 - 10
+            m1 - 10
 
         with self.assertRaises(ValueError):
-            _ = m1 - 0.5
-
-        with self.assertRaises(ValueError):
-            _ = m1 - "0.5"
+            m1 - 0.5
 
     def test_mul(self):
         m1 = Matrix((1, 2), (3, 4))
@@ -102,71 +97,41 @@ class TestMatrix(unittest.TestCase):
         self.assertEqual(0.5 * m1, Matrix((0.5, 1), (1.5, 2)))
 
         with self.assertRaises(ValueError):
-            _ = m1 * m3
+            m1 * m3
 
-        with self.assertRaises(ValueError):
-            _ = m1 * "hi"
-
-        with self.assertRaises(ValueError):
-            _ = m1 * [1, 2, 3]
-
-        with self.assertRaises(ValueError):
-            _ = m1 * ["hi", "i'm", "a test"]
-
-        with self.assertRaises(ValueError):
-            _ = m1 * (1, 2)
-
-        with self.assertRaises(ValueError):
-            _ = m1 * ("hi", "i'm", "a test")
+        with self.assertRaises(TypeError):
+            m1 * (1, 2)
 
     def test_div(self):
         m1 = Matrix((1, 2), (3, 4))
         m2 = Matrix((5, 6), (7, 8))
         m3 = Matrix.ones(4)
 
-        self.assertEqual(m1 / m2, Matrix((0.2, 2 / 6), (3 / 7, 0.5)))
         self.assertEqual(m1 / 10, Matrix((0.1, 0.2), (0.3, 0.4)))
         self.assertEqual(m1 / 0.5, Matrix((2, 4), (6, 8)))
 
         with self.assertRaises(ValueError):
-            _ = m1 / m3
+            m1 / m2
 
         with self.assertRaises(ValueError):
-            _ = m1 / "hi"
+            m1 / m3
 
-        with self.assertRaises(ValueError):
-            _ = m1 / [1, 2, 3]
-
-        with self.assertRaises(ValueError):
-            _ = m1 / ["hi", "i'm", "a test"]
-
-        with self.assertRaises(ValueError):
-            _ = m1 / (1, 2)
-
-        with self.assertRaises(ValueError):
-            _ = m1 / ("hi", "i'm", "a test")
+        with self.assertRaises(TypeError):
+            m1 / (1, 2)
 
     def test_equal(self):
-        self.assertTrue(
-            Matrix.unity(3) == Matrix((1, 0, 0), (0, 1, 0), (0, 0, 1)))
+        self.assertEqual(
+            Matrix.unity(3), Matrix((1, 0, 0), (0, 1, 0), (0, 0, 1)))
 
-        self.assertFalse(Matrix((1, 2), (3, 4)) == Matrix((5, 6), (7, 8)))
-        self.assertFalse(Matrix((1, 2), (3, 4)) == Matrix.ones(3))
+        self.assertEqual(
+            Matrix.ones(2), Matrix((1, 1), (1, 1)))
 
     def test_not_equal(self):
-        self.assertTrue(
-            Matrix.unity(3) != Matrix.ones(3))
-
-        self.assertTrue(
-            Matrix.unity(3) != Matrix.ones(4))
-
-        self.assertFalse(Matrix((1, 2), (3, 4)) != Matrix((1, 2), (3, 4)))
+        self.assertNotEqual(Matrix((1, 2), (3, 4)), Matrix((5, 6), (7, 8)))
+        self.assertNotEqual(Matrix((1, 2), (3, 4)), Matrix.ones(3))
 
     def test_iteration(self):
-        self.assertNotEqual(list(iter(Matrix.ones(3))), [])
-
-        for line in Matrix.ones(2):
-            self.assertEqual(line, (1, 1))
+        self.assertEqual(tuple(Matrix.ones(2)), ((1, 1), (1, 1)))
 
     def test_hash(self):
         d = {Matrix((1, 1), (2, 2)): 2, Matrix((1, 1), (2, 3)): 3}
