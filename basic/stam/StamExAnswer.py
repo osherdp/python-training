051c6715c5bcccff66daac43b766baa38485f439
Karoline123121 <keren084@gmail.com>
2020-03-24 21:42:42 +0200

    -I fixed the problematic Docstrings -I changed the try and except to a condition that checks if file paths exist -I added the output file path as an argument to the function -I removed unnecessary print line

--- basic/stam/StamExAnswer.py
+++ basic/stam/StamExAnswer.py
@@ -1,42 +1,50 @@
-__author__ = "Keren"
+"""This program reads a file, edits it and saves it in a new location"""
+import os
 
 FIRST_NAME = "Keren"
 LAST_NAME = "Guez"
 
 
 def add_vals(line):
-    """
-    Args:
-        line: (str) a line of text
+    """This function concatenates a line with some given text
 
-    Returns: a new concatenated line with constant values
+    Args:
+        line (str): a line of text
+
+    Returns:
+        str: a new concatenated line
 
     """
     return FIRST_NAME + line + LAST_NAME
 
 
-def stam(filename):
-    """ The function gets file path, edits it and saves the new edited file to a new location
+def stam(filename, output_file):
+    """The function gets a file, edits it and saves it to a new location
 
     Args:
-        filename: (str) the full path to the file that will be edited
+        filename (str): the full path to the file that will be edited
+        output_file (str) : the path of the new file to be saved
 
     """
-    try:
+    last_backslash = output_file.rfind("\\")
+    if os.path.exists(filename) and os.path.exists(output_file[:last_backslash]):
         with open(filename, 'r') as file:
-            new_concat_lines_lis = (add_vals(line[:-1])+"\n" if line[-1:] == "\n" else add_vals(line) for line in file)
-            new_path_to_save = r"C:\Users\keren\Desktop\newfile.txt"
-            with open(new_path_to_save, 'w') as new_file:
+            new_concat_lines_lis = (add_vals(line[:-1])+"\n"
+                                    if line[-1:] == "\n"
+                                    else add_vals(line)
+                                    for line in file)
+            with open(output_file, 'w') as new_file:
                 for line in new_concat_lines_lis:
-                    print("line "+line, end="")
                     new_file.write(line)
-    except Exception as e:
-        print(e)
+    else:
+        print(f"either the original file path or the output path don't exist")
+
 
 
 def main():
     path = r"C:\Users\keren\Desktop\rooms.txt"
-    stam(path)
+    new_path_to_save = r"C:\Users\keren\Desktop\newfile2.txt"
+    stam(path, new_path_to_save)
 
 
 if __name__ == "__main__":
