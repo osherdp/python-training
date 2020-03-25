ac5020b3d4dd2c1ddfb6fc1ec3cd2200366b896a
Yonatan Evantal <yonatan.evantal@gmail.com>
2020-03-25 16:42:32 +0200

    Doc-strings and seperation of tests

--- matrix/test_unittest_matrix.py
+++ matrix/test_unittest_matrix.py
@@ -7,20 +7,14 @@ mathematical operations, boolean operations, iterations and is hashable.
 
 import unittest
 
-import pytest
 from matrix import Matrix
 
 
-@pytest.fixture(scope="module")
-def db_class(request):
-    # set a class attribute on the invoking test context
-    request.cls.db = Matrix()
-
-
-class TestMatrix(unittest.TestCase):
-    """Unit tests for Matrix class."""
-
+class TestMatrixCreate(unittest.TestCase):
+    """Test the creation of a matrix and that an instance can't be modified.
+    """
     def test_initialization(self):
+        """Test init function of Matrix."""
         self.assertIsInstance(Matrix((1, 2), (3, 4)), Matrix)
         self.assertIsInstance(Matrix((1, 2), (5.5, 4)), Matrix)
 
@@ -31,24 +25,34 @@ class TestMatrix(unittest.TestCase):
             Matrix((1, 2), (3, 4, 5))
 
     def test_modification(self):
+        """Test that Matrix attribute can't be modified."""
         m = Matrix((1, 1), (2, 2))
 
         with self.assertRaises(TypeError):
             m._matrix[0] = (1, 2)
 
-    def test_str(self):
+
+class TestMatrixFunctions(unittest.TestCase):
+    """Test the built-in functions of Matrix."""
+    def test_representation(self):
+        """Test __str__  and __repr__ functions."""
         data = (1, 1, 1), (2, 2, 2), (3, 3, 3)
         m = Matrix(*data)
 
         self.assertEqual(str(m), str(data))
+        self.assertEqual(repr(m), f'<Matrix object {hash(m)}>')
 
     def test_tuples(self):
+        """Test tuple which should return the matrix data as tuple."""
         data = (1, 1), (2, 2)
         m = Matrix(*data)
 
         self.assertEqual(m.tuples, data)
 
     def test_unity(self):
+        """Test unity() which should return a matrix with 1's in the main
+        diagonal and 0's elsewhere.
+        """
         self.assertEqual(
             Matrix.unity(2), Matrix((1, 0), (0, 1)))
 
@@ -56,13 +60,18 @@ class TestMatrix(unittest.TestCase):
             Matrix.unity(3), Matrix((1, 0, 0), (0, 1, 0), (0, 0, 1)))
 
     def test_ones(self):
+        """Test ones() which should return a matrix full of 1's."""
         self.assertEqual(
             Matrix.ones(2), Matrix((1, 1), (1, 1)))
 
         self.assertEqual(
             Matrix.ones(3), Matrix((1, 1, 1), (1, 1, 1), (1, 1, 1)))
 
+
+class TestMatrixOperators(unittest.TestCase):
+    """Test the mathematical and boolean operations supported by Matrix."""
     def test_add(self):
+        """Test addition operator."""
         m1 = Matrix.ones(3)
         m2 = Matrix((11, 12, 13), (14, 15, 16), (17, 18, 19))
         m3 = Matrix.ones(2)
@@ -80,6 +89,7 @@ class TestMatrix(unittest.TestCase):
             m1 + 0.5
 
     def test_sub(self):
+        """Test subtraction operator."""
         m1 = Matrix((1, 2), (3, 4))
         m2 = Matrix((5, 6), (7, 8))
         m3 = Matrix.ones(3)
@@ -96,6 +106,7 @@ class TestMatrix(unittest.TestCase):
             m1 - 0.5
 
     def test_mul(self):
+        """Test multiplication and reverse mul operators."""
         m1 = Matrix((1, 2), (3, 4))
         m2 = Matrix((5, 6), (7, 8))
         m3 = Matrix.ones(4)
@@ -115,6 +126,7 @@ class TestMatrix(unittest.TestCase):
             m1 * (1, 2)
 
     def test_div(self):
+        """Test true division operator operator."""
         m1 = Matrix((1, 2), (3, 4))
         m2 = Matrix((5, 6), (7, 8))
         m3 = Matrix.ones(4)
@@ -132,6 +144,7 @@ class TestMatrix(unittest.TestCase):
             m1 / (1, 2)
 
     def test_equal(self):
+        """Test equal boolean operator."""
         self.assertEqual(
             Matrix.unity(3), Matrix((1, 0, 0), (0, 1, 0), (0, 0, 1)))
 
@@ -139,13 +152,22 @@ class TestMatrix(unittest.TestCase):
             Matrix.ones(2), Matrix((1, 1), (1, 1)))
 
     def test_not_equal(self):
+        """Test not equal boolean operator."""
         self.assertNotEqual(Matrix((1, 2), (3, 4)), Matrix((5, 6), (7, 8)))
         self.assertNotEqual(Matrix((1, 2), (3, 4)), Matrix.ones(3))
 
+
+class TestMatrixIteration(unittest.TestCase):
+    """Test iteration support of Matrix."""
     def test_iteration(self):
+        """Test __iter__ function."""
         self.assertEqual(tuple(Matrix.ones(2)), ((1, 1), (1, 1)))
 
+
+class TestMatrixHashable(unittest.TestCase):
+    """Test hash support of Matrix."""
     def test_hash(self):
+        """Test __hash__ function and uses dictionary keys."""
         d = {Matrix((1, 1), (2, 2)): 2, Matrix((1, 1), (2, 3)): 3}
 
         self.assertEqual(d, {Matrix((1, 1), (2, 2)): 2,
