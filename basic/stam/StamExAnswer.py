655ddafbdb06be0f693bdd4f9f5bccad997f43e0
Karoline123121 <keren084@gmail.com>
2020-03-25 16:51:58 +0200

    fixed the mistakes

--- basic/stam/StamExAnswer.py
+++ basic/stam/StamExAnswer.py
@@ -1,47 +1,49 @@
-"""This program reads a file, edits it and saves it in a new location"""
+"""Reads a file, edits it and saves it in a new location."""
 import os
+
 FIRST_NAME = "Keren"
 LAST_NAME = "Guez"
 
 
-def add_vals(line):
-    """This function concatenates a line with some given text
+def check_exists(input_file, output_folder):
+    """Checks if paths are valid and prints a message accordingly.
 
     Args:
-        line (str): a line of text
-
+        input_file (str): The path to the file that will be edited.
+        output_folder (str) : The path to the output folder.
     Returns:
-        str: a new concatenated line
-
+        True : If both paths are valid.
+        False : If any path isn't valid.
     """
-    return FIRST_NAME + line + LAST_NAME
+    if not os.path.exists(input_file):
+        print(f"The input file '{input_file}' doesn't exist.")
+        return False
+    if not os.path.exists(output_folder):
+        print(f"The output folder '{output_folder}' doesn't exist.")
+        return False
+    return True
 
 
 def stam(filename, output_file):
-    """The function gets a file, edits it and saves it to a new location
+    """Gets a file, edits it and saves it to a new location.
 
     Args:
-        filename (str): the path to the file that will be edited
-        output_file (str) : the path of the new file to be saved
-
+        filename (str): The path to the file that will be edited.
+        output_file (str) : The path of the new file to be saved.
     """
-    last_slash = output_file.rfind("\\")
-    if os.path.exists(filename) and os.path.exists(output_file[:last_slash]):
+    output_dir = os.path.dirname(os.path.abspath(output_file))
+    if check_exists(filename, output_dir):
         with open(filename, 'r') as file:
-            new_concat_lines_lis = (add_vals(line[:-1])+"\n"
-                                    if line[-1:] == "\n"
-                                    else add_vals(line)
-                                    for line in file)
+            new_concat_lines = (FIRST_NAME + line[:-1] + LAST_NAME + "\n"
+                                for line in file)
             with open(output_file, 'w') as new_file:
-                for line in new_concat_lines_lis:
+                for line in new_concat_lines:
                     new_file.write(line)
-    else:
-        print(f"either the original file path or the output path don't exist")
 
 
 def main():
     path = r"C:\Users\keren\Desktop\rooms.txt"
-    new_path_to_save = r"C:\Users\keren\Desktop\newfile2.txt"
+    new_path_to_save = r"C:\Users\keren\Desktop\newfile3.txt"
     stam(path, new_path_to_save)
 
 
