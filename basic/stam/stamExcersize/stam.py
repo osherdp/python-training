82d2e37e0612abd81539ee48770b0fba71b58232
galyammaya <galyammaya@gmail.com>
2020-03-25 20:24:02 +0200

    Stam fixed

--- basic/stam/stamExcersize/stam.py
+++ basic/stam/stamExcersize/stam.py
@@ -1,5 +1,3 @@
-__author__ = 'Maya Gal-Yam'
-
 import sys
 import os
 from os import path
@@ -9,26 +7,26 @@ NEW_LINE_FORMAT = 'Maya {} Gal-Yam\n'
 NEW_FILE_NAME = 'newfile.txt'
 
 
-def get_file():
-    """gets the files passed in if args are valid"""
-    try:
-        originalfile_path = sys.argv[ORIGINAL_FILE_LOCATION]
-        if path.exists(originalfile_path) and path.isfile(originalfile_path):
-            file_to_get = open(originalfile_path, 'r')
-        else:
-            print("Given path isn't a file")
-            sys.exit()
-    except IndexError:
-        print('No path given')
-        sys.exit()
-    return file_to_get
-
-
-def create_new():
-    """opens/creates new file in the working directory"""
-    newfile_path = path.join(os.getcwd(), NEW_FILE_NAME)
-    file_to_create = open(newfile_path, 'w')
-    return file_to_create
+# def get_file():
+#     """gets the files passed in if args are valid"""
+#     try:
+#         originalfile_path = sys.argv[ORIGINAL_FILE_LOCATION]
+#         if path.exists(originalfile_path) and path.isfile(originalfile_path):
+#             file_to_get = open(originalfile_path, 'r')
+#         else:
+#             print("Given path isn't a file")
+#             sys.exit()
+#     except IndexError:
+#         print('No path given')
+#         sys.exit()
+#     return file_to_get
+#
+#
+# def create_new():
+#     """opens/creates new file in the working directory"""
+#     newfile_path = path.join(os.getcwd(), NEW_FILE_NAME)
+#     file_to_create = open(newfile_path, 'w')
+#     return file_to_create
 
 
 def manipulate(original, new):
@@ -40,10 +38,22 @@ def manipulate(original, new):
 
 
 def main():
-    original_file = get_file()
-    new_file = create_new()
-    manipulate(original_file, new_file)
+    try:
+        original_file = open(originalfile_path, 'r')
+        new_file = open(newfile_path, 'w')
+        manipulate(original_file, new_file)
+        original_file.close()
+        new_file.close()
+    except FileNotFoundError:
+        print("Given path isn't a file")
+        sys.exit()
 
 
 if __name__ == '__main__':
+    try:
+        originalfile_path = sys.argv[ORIGINAL_FILE_LOCATION]
+        newfile_path = path.join(os.getcwd(), NEW_FILE_NAME)
+    except IndexError:
+        print('No path given')
+        sys.exit()
     main()
