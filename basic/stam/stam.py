12b620017a47f421ffbb4f55edf649d58957b3d0
YNurick <Yuval.nurick@gmail.com>
2020-03-26 16:44:44 +0200

    pep 8, 2'nd try

--- basic/stam/stam.py
+++ basic/stam/stam.py
@@ -10,16 +10,18 @@ def clear_file(path):
 
 
 def main():
-    """take file name from argument and copies it to file name in second argument
-    in addition - adds 'first_name' to the beginning if each line and 'last_name' to the end of each line"""
+    """take file name from argument and copies it to file name in second
+    argument in addition - adds 'first_name' to the beginning םf each line
+    and 'last_name' to the end of each line """
     path = '/'.join(sys.argv[0].split('/')[:-1]) + '/'
     try:
         org_file_name = sys.argv[1]
-    except:
+    except IndexError:
+        print(Exception)
         org_file_name = 'README.md'
     try:
         new_file_name = sys.argv[2]
-    except:
+    except IndexError:
         new_file_name = 'solution.md'
 
     org_file = path + org_file_name
@@ -32,7 +34,8 @@ def main():
     with open(org_file, 'r') as ofh:
         with open(new_file, 'a') as nfh:
             for line in ofh:
-                nfh.write(first_name + ' ' + line.rstrip() + ' ' + last_name + '\n')
+                nfh.write(first_name + ' ' + line.rstrip() + ' ' + last_name
+                          + '\n')
 
 
 if __name__ == '__main__':
