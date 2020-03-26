90398eeebf15f4825708f45ef3b44a5f37b9d44b
Karoline123121 <keren084@gmail.com>
2020-03-26 18:47:16 +0200

    stam fixed

--- basic/stam/StamExAnswer.py
+++ basic/stam/StamExAnswer.py
@@ -1,50 +1,34 @@
 """Reads a file, edits it and saves it in a new location."""
-import os
+import sys
 
+INPUT_FILE = 1
+OUTPUT_FILE = 2
 FIRST_NAME = "Keren"
 LAST_NAME = "Guez"
 
 
-def check_exists(input_file, output_folder):
-    """Checks if paths are valid and prints a message accordingly.
-
-    Args:
-        input_file (str): The path to the file that will be edited.
-        output_folder (str) : The path to the output folder.
-    Returns:
-        True : If both paths are valid.
-        False : If any path isn't valid.
-    """
-    if not os.path.exists(input_file):
-        print(f"The input file '{input_file}' doesn't exist.")
-        return False
-    if not os.path.exists(output_folder):
-        print(f"The output folder '{output_folder}' doesn't exist.")
-        return False
-    return True
-
-
 def stam(filename, output_file):
-    """Gets a file, edits it and saves it to a new location.
+    """Get a file, edit it and save it to a new location.
 
     Args:
         filename (str): The path to the file that will be edited.
         output_file (str) : The path of the new file to be saved.
     """
-    output_dir = os.path.dirname(os.path.abspath(output_file))
-    if check_exists(filename, output_dir):
-        with open(filename, 'r') as file:
-            new_concat_lines = (FIRST_NAME + line[:-1] + LAST_NAME + "\n"
-                                for line in file)
-            with open(output_file, 'w') as new_file:
-                for line in new_concat_lines:
-                    new_file.write(line)
+    with open(filename, 'r') as file:
+        with open(output_file, 'w') as new_file:
+            for line in file:
+                new_line = f"{FIRST_NAME}{line[:-1]}{LAST_NAME}\n"
+                new_file.write(new_line)
 
 
 def main():
-    path = r"C:\Users\keren\Desktop\rooms.txt"
-    new_path_to_save = r"C:\Users\keren\Desktop\newfile3.txt"
-    stam(path, new_path_to_save)
+    """Initialize input and output paths and call stam function."""
+    try:
+        path = sys.argv[INPUT_FILE]
+        new_path_to_save = sys.argv[OUTPUT_FILE]
+        stam(path, new_path_to_save)
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
