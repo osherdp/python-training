f9ba132bf91abf76e0dee8421c9725be51f60912
idans124 <idans124@gmail.com>
2020-03-26 11:50:44 +0200

    stam Ex

--- Pr1/stam.py
+++ Pr1/stam.py
@@ -2,16 +2,22 @@ import sys
 
 
 FILE_NAME = 1
+FILE_NAME_NEW = "D:/Python Training/Pr1/stam_new.txt"
 
 
-def write_first_name(file_name, first_name):
-    with open(file_name, 'a') as first_name_input:
-        first_name_input.write(first_name + ', ')
+def write_to_file(file_name, FILE_NAME_NEW, first_name, last_name):
+    """ Read from file stam.txt line by line, and write to file stam_new.txt with the first and last name
+    """
 
-
-def write_last_name(file_name, last_name):
-    with open(file_name, 'a') as last_name_input:
-        last_name_input.write(last_name)
+    try: # if stam.txt exist
+        with open(file_name, 'r') as orig_data:
+            for line in orig_data:
+                # print(line.strip())
+                line = line.strip()
+                with open(FILE_NAME_NEW, 'a') as new_file:
+                    new_file.write(first_name + ' ' + line.replace("\n", " ") + ' ' + last_name + '\n')
+    except Exception as err:  # if stam.txt doesn't exist
+        print(err)  # print the error
 
 
 def receive_first_name():
@@ -24,10 +30,9 @@ def receive_last_name():
 
 def main():
     file_name = sys.argv[FILE_NAME]
-    # first_name = receive_first_name()
-    # last_name = receive_last_name()
-    write_first_name(file_name, 'Idan')
-    write_last_name(file_name, 'Sokolovsky')
+    first_name = receive_first_name()
+    last_name = receive_last_name()
+    write_to_file(file_name, FILE_NAME_NEW, first_name, last_name)
 
 
 if __name__ == "__main__":
