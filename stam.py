b9de308bfc2301a1badb2868bf46dbdb1f806106
yahillel <yahillel@gmail.com>
2020-03-28 22:48:42 +0300

    Fixed lines
    lines are now shorter than 80 charaters

--- stam.py
+++ stam.py
@@ -9,18 +9,20 @@ OUTPUT_FILENAME_ARG_NUM = 2
 def add_name_to_line(text_lines, pre_text, succeeding_text):
     """
     This function adds the string received in pre_text to the beginning of each
-    line in text_lines list of strings and adds succeeding_text to the end of each line
+    line in text_lines list of strings and adds succeeding_text to
+    the end of each line
     """
     for ind in range(len(text_lines)):
-        text_lines[ind] = pre_text+' '+text_lines[ind].rstrip('\n')+' '+succeeding_text+'\n'
+        text_lines[ind] = pre_text+' '+text_lines[ind].rstrip('\n')\
+                          + ' '+succeeding_text+'\n'
     return text_lines
 
 
 def main():
     """
-    This python file copies the contents of the file given in the first argument
-    into the file given in the second argument while adding my first name before
-    each line and last name after the line
+    This python file copies the contents of the file given in the first
+    argument into the file given in the second argument while adding
+    my first name before each line and last name after the line
     """
     try:
         input_f_name = sys.argv[INPUT_FILENAME_ARG_NUM]
