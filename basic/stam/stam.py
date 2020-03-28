188148a79fcaba763e91ebefc45fdc5959cba7eb
YNurick <Yuval.nurick@gmail.com>
2020-03-28 17:15:38 +0300

    3'rd fix

--- basic/stam/stam.py
+++ basic/stam/stam.py
@@ -6,7 +6,7 @@ def main():
     """take file name from argument and copies it to file name in second
     argument in addition - adds 'first_name' to the beginning םf each line
     and 'last_name' to the end of each line """
-    path = os.path.dirname(sys.argv[0]) + '/'
+    path = os.path.dirname(sys.argv[0])
     try:
         org_file_name = sys.argv[1]
     except IndexError:
@@ -18,17 +18,17 @@ def main():
         print('Not enough input arguments, using solution.md as output file')
         new_file_name = 'solution.md'
 
-    org_file = path + org_file_name
-    new_file = path + new_file_name
+    org_file = os.path.join(path, org_file_name)
+    new_file = os.path.join(path, new_file_name)
 
     first_name = 'Yuval'
     last_name = 'Nurick'
 
-    # ofh is original file handler and nfh is new file  handler
-    with open(org_file, 'r') as ofh:
-        with open(new_file, 'w') as nfh:
-            for line in ofh:
-                nfh.write(f'{first_name} {line.rstrip()} {last_name}\n')
+    with open(org_file, 'r') as org_file_handler:
+        with open(new_file, 'w') as new_file_handler:
+            for line in org_file_handler:
+                new_file_handler.write(f'{first_name} {line.rstrip()} '
+                                       f'{last_name}\n')
 
 
 if __name__ == '__main__':
