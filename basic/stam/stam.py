fd0692f97b23d021f5a3b44239f1a376f008c2ba
YNurick <Yuval.nurick@gmail.com>
2020-03-28 16:39:36 +0300

    fixed by comments

--- basic/stam/stam.py
+++ basic/stam/stam.py
@@ -1,27 +1,21 @@
 import sys
-
-__author__ = 'Nurick'
-
-
-def clear_file(path):
-    """delete a file content"""
-    with open(path, 'w') as fh:
-        fh.write('')
+import os
 
 
 def main():
     """take file name from argument and copies it to file name in second
     argument in addition - adds 'first_name' to the beginning םf each line
     and 'last_name' to the end of each line """
-    path = '/'.join(sys.argv[0].split('/')[:-1]) + '/'
+    path = os.path.dirname(sys.argv[0]) + '/'
     try:
         org_file_name = sys.argv[1]
     except IndexError:
-        print(Exception)
+        print('Not enough input arguments, using README.MD as input file')
         org_file_name = 'README.md'
     try:
         new_file_name = sys.argv[2]
     except IndexError:
+        print('Not enough input arguments, using solution.md as output file')
         new_file_name = 'solution.md'
 
     org_file = path + org_file_name
@@ -30,12 +24,11 @@ def main():
     first_name = 'Yuval'
     last_name = 'Nurick'
 
-    clear_file(new_file)
+    # ofh is original file handler and nfh is new file  handler
     with open(org_file, 'r') as ofh:
-        with open(new_file, 'a') as nfh:
+        with open(new_file, 'w') as nfh:
             for line in ofh:
-                nfh.write(first_name + ' ' + line.rstrip() + ' ' + last_name
-                          + '\n')
+                nfh.write(f'{first_name} {line.rstrip()} {last_name}\n')
 
 
 if __name__ == '__main__':
