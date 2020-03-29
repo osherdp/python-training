737a4f607dbc432f709f8376b7832910c5fb815a
yahillel <yahillel@gmail.com>
2020-03-29 14:48:52 +0300

    Fixed PEP8 errors
    added spaces and exeption printing

--- stam.py
+++ stam.py
@@ -7,16 +7,14 @@ OUTPUT_FILENAME_ARG_NUM = 2
 FIRST_NAME = 'Yaron'
 LAST_NAME = 'Cohen'
 
+
 def add_name_to_line(text_lines, pre_text, succeeding_text):
     """
     This function adds the string received in pre_text to the beginning of each
     line in text_lines list of strings and adds succeeding_text to
     the end of each line
     """
-    # for ind in range(len(text_lines)):
-    #     text_lines[ind] = pre_text+' '+text_lines[ind].rstrip('\n')\
-    #                       + ' '+succeeding_text+'\n'
-    out_text = ['{} {} {}\n'.format(pre_text,line.rstrip('\n'),succeeding_text)
+    out_text = ['{} {} {}\n'.format(pre_text, line.rstrip('\n'), succeeding_text)
                 for line in text_lines]
     return out_text
 
@@ -30,10 +28,11 @@ def main():
     try:
         input_f_name = sys.argv[INPUT_FILENAME_ARG_NUM]
         output_f_name = sys.argv[OUTPUT_FILENAME_ARG_NUM]
-    except:
-        print('Error - Not enough inputs')
+    except Exception as e:
+        print(e)
+        print('This simply means not enough inputs')
         return
-    with open(input_f_name,'r') as in_file:
+    with open(input_f_name, 'r') as in_file:
         text = in_file.readlines()
 
     new_text = add_name_to_line(text, FIRST_NAME, LAST_NAME)
