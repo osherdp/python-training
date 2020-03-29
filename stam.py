dac601b252bfb898da29d53bd4de6fb2c50c95ca
AdiKadarLevi <62499966+AdiKadarLevi@users.noreply.github.com>
2020-03-29 19:06:22 +0300

    Update stam.py
    29.3.2020 19:06 corrections

--- stam.py
+++ stam.py
@@ -6,13 +6,15 @@ LAST_NAME = 'Kadar Levi'
 
 def main():
     try:
-        input_file_path = input('please enter text file path\n')
+        input_file_path: str = input('please enter text file path\n')
         with open(input_file_path, 'r') as input_file:
             with open('new_text.txt', 'w+') as new_file:
                 for line in input_file:
-                    new_line = FIRST_NAME + ' ' + format(line[:-1]) + ' ' + LAST_NAME
+                    new_line = ' {} {} {}'.format(FIRST_NAME, line.strip(),  LAST_NAME)
                     print(new_line)
-                    new_file.write(new_line + '\n')
+                    new_file.writelines(new_line)
+    except FileNotFoundError:
+        print('Empty Path')
     except Exception as e:
         print(e)
 
