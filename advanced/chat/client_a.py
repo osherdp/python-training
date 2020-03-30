75cf78faf1a6240630b56617eb362809bd920dea
snir2121 <snirch1461@gmail.com>
2020-03-30 22:47:34 +0300

    Added chat exercise. It's implementation is different then the instructions. To start the chat we need to run each script in the cmd. server first, then clients (one bye one). The chat can work with more then one client (The implementation is the same for every client).

---
+++ advanced/chat/client_a.py
@@ -0,0 +1,56 @@
+# Python TCP Client A
+import socket
+from threading import Thread
+
+
+def write(tcp_client_a):
+    """
+    This func runs a loop that waits for a text from the client and send it to the server.
+    When "exit" is entered, the loop stops.
+    :param tcp_client_a: The client.
+    :return:
+    """
+    exit_flag = False
+    msg = input("tcpClientA: Enter message/ Enter exit:\n")
+    while not exit_flag:
+        if msg == "exit":
+            exit_flag = True
+        tcp_client_a.send(bytes(msg, "utf-8"))
+        msg = input("")
+
+
+def read(tcp_client_a, buffer_size):
+    """
+    The func receives data from the server and prints it.
+    :param tcp_client_a: The client.
+    :param buffer_size: The size of the buffer of the data that received.
+    """
+    while True:
+        data = tcp_client_a.recv(buffer_size).decode("utf-8")
+        print(data)
+
+
+def main():
+    """
+    Sets the parameters of the client, and starts the write() and read() functions in 2 threads.
+    """
+    # If working in different computers the host name should be the same as the server host name.
+    host = socket.gethostname()
+    port = 2004
+    buffer_size = 2000
+
+    tcp_client_a = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_client_a.connect((host, port))
+
+    # Write thread.
+    w_thread = Thread(target=write, args=(tcp_client_a,))
+
+    # Read thread.
+    r_thread = Thread(target=read, args=(tcp_client_a, buffer_size))
+
+    w_thread.start()
+    r_thread.start()
+
+
+if __name__ == '__main__':
+    main()
