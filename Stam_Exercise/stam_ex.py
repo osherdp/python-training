a245cee627e61dbe7c4397f944b5b2742f9cc800
lielya <liel.nana1@gmail.com>
2020-03-30 22:06:08 +0300

    solved stam exercise

---
+++ Stam_Exercise/stam_ex.py
@@ -0,0 +1,28 @@
+import sys
+
+FILE1 = 1
+FILE2 = 2
+
+
+def main():
+    try:
+        with open(sys.argv[FILE1], 'r') as f1:
+            with open(sys.argv[FILE2], 'w') as f2:
+                f_content = f1.read().split('\n')
+                print(f_content)
+
+                new_content = (list(map(lambda x: 'liel' + ' ' + x + ' ' + 'yaakobov', f_content)))
+                print(new_content)
+
+                for i in new_content:
+                    #print(i)
+                    f2.writelines("{}\n".format(i))
+
+        with open(sys.argv[FILE2], 'r') as f:
+            print(f.read())
+
+    except Exception as e:
+        print(e)
+
+if __name__ == '__main__':
+    main()
