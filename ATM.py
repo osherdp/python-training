ea2afa490f5e33d955400a2863d4067ce8a04c4f
AdiKadarLevi <62499966+AdiKadarLevi@users.noreply.github.com>
2020-03-31 13:02:00 +0300

    Update ATM.py

--- ATM.py
+++ ATM.py
@@ -1,80 +1,96 @@
 __author__ = 'Adi Kadar Levi'
-BANK_DATA_PATH = r'C:\Users\Elior Mor Yosef\Documents\GitHub' \
-                 r'\python-training\python-training\bank.txt'
+
+class ATM:
+    def __init__(self):
+        self.id_num = 0
+        self.password = 0
+        self.balance = 0
+        self.__pass_dict = {}
+        self.__bal_dict = {}
+
+    def get_passwords_dictionary(self):
+        bank_data_path = r'C:\Users\Elior Mor Yosef\Documents\GitHub' \
+                         r'\python-training\python-training\bank.txt'
+        pass_dict = {}
+        with open(bank_data_path, 'r') as bank_data:
+            for line in bank_data:
+                words = line.split()
+                self.__pass_dict[words[0]] = words[1]
+        return self.__pass_dict
 
 
-def passwords_dictionary():
-    pass_dict = {}
-    with open(BANK_DATA_PATH, 'r') as bank_data:
-        for line in bank_data:
-            words = line.split()
-            pass_dict[words[0]] = words[1]
-    return pass_dict
+    def get_balance_dictionary(self):
+        bank_data_path = r'C:\Users\Elior Mor Yosef\Documents\GitHub' \
+                         r'\python-training\python-training\bank.txt'
+        bal_dict = {}
+        with open(bank_data_path, 'r') as bank_data:
+            for line in bank_data:
+                words = line.split()
+                self.__bal_dict[words[0]] = words[2]
+        return self.__bal_dict
 
 
-def balance_dictionary():
-    balance_dict = {}
-    with open(BANK_DATA_PATH, 'r') as bank_data:
-        for line in bank_data:
-            words = line.split()
-            balance_dict[words[0]] = words[2]
-    return balance_dict
-
-
-def check_id(id_input):
-    if id_input not in passwords_dictionary().keys():
-        print('User Not Found')
-        return False
-    else:
+    def check_id(self, id_input):
+        if id_input not in self.get_passwords_dictionary().keys():
+            print('User Not Found')
+            return False
         return True
 
 
-def check_password(user_id, input_password, pass_data):
-    while pass_data[user_id] != input_password:
-        print('password incorrect\n')
-        input_password = input('please enter your password\n')
+    def check_password(self,user_id, input_password, pass_data):
+        while pass_data[user_id] != input_password:
+            print('password incorrect\n')
+            input_password = input('please enter your password\n')
 
 
-def check_balance(id_num, balance_data):
-    balance = balance_data[id_num]
-    return float(balance)
-    pass
+    def get_balance(self, id_num, balance_data):
+        self.balance = balance_data[id_num]
+        return float(self.balance)
+        pass
 
 
-def withdraw(user_id, amount_to_withdraw, pass_dict, balance_dict):
-    balance_dict[user_id] = float(balance_dict[user_id]) -\
-                            float(amount_to_withdraw)
-    with open(BANK_DATA_PATH, 'w') as new_bank:
-        for key in pass_dict:
-            print(key, pass_dict[key], balance_dict[key], file=new_bank)
-    pass
+    def set_withdraw(self, user_id, amount_to_withdraw, pass_dict, balance_dict):
+        bank_data_path = r'C:\Users\Elior Mor Yosef\Documents\GitHub' \
+                         r'\python-training\python-training\bank.txt'
 
+        balance_dict[user_id] = float(balance_dict[user_id]) -\
+                                float(amount_to_withdraw)
+        with open(bank_data_path, 'w') as new_bank:
+            for key in pass_dict:
+                print(key, pass_dict[key], balance_dict[key], file=new_bank)
+        pass
 
-def deposit(user_id, amount_to_deposit, pass_dict, balance_dict):
-    balance_dict[user_id] = float(amount_to_deposit) + \
-                            float(balance_dict[user_id])
-    with open(BANK_DATA_PATH, 'w') as new_bank:
-        for key in pass_dict:
-            print(key, pass_dict[key], balance_dict[key], file=new_bank)
-    pass
+    def set_deposit(self, user_id, amount_to_deposit, pass_dict, balance_dict):
+        bank_data_path = r'C:\Users\Elior Mor Yosef\Documents\GitHub' \
+                         r'\python-training\python-training\bank.txt'
+        balance_dict[user_id] = float(amount_to_deposit) + \
+                                float(balance_dict[user_id])
+        with open(bank_data_path, 'w') as new_bank:
+            for key in pass_dict:
+                print(key, pass_dict[key], balance_dict[key], file=new_bank)
+        pass
 
+    def set_change_password(self, new_password, user_id, pass_dict, balance_dict):
+        bank_data_path = r'C:\Users\Elior Mor Yosef\Documents\GitHub' \
+                             r'\python-training\python-training\bank.txt'
 
-def change_password(new_password, user_id, pass_dict, balance_dict):
-    pass_dict[user_id] = new_password
-    with open(BANK_DATA_PATH, 'w') as new_bank:
-        for key in pass_dict:
-            print(key, pass_dict[key], balance_dict[key], file=new_bank)
-    pass
+        pass_dict[user_id] = new_password
+        with open(bank_data_path, 'w') as new_bank:
+            for key in pass_dict:
+                print(key, pass_dict[key], balance_dict[key], file=new_bank)
+        pass
 
 
 def main():
-    pass_dict = passwords_dictionary()
-    bal_dict = balance_dictionary()
+    user = ATM()
+
+    pass_dict = user.get_passwords_dictionary()
+    bal_dict = user.get_balance_dictionary()
     print('hello and welcome to The Thieves ATM!')
     id_num = input('please enter you ID number\n')
-    if check_id(id_num):
+    if user.check_id(id_num):
         password = input('please enter your password\n')
-        check_password(id_num, password, pass_dict)
+        user.check_password(id_num, password, pass_dict)
         print('What would you like to do today?\n'
               'To check your balance press 1\n'
               'To deposit money press 2\n'
@@ -84,8 +100,8 @@ def main():
         choose = input('\n')
         while choose != '-1':
             if choose == '1':
-                balance = check_balance(id_num, bal_dict)
-                print('your balance is {}' .format(balance))
+                balance = user.get_balance(id_num, bal_dict)
+                print('your balance is {}'.format(balance))
                 print('Thank you for using our sevices\n'
                       'For this action '
                       'you will be charged for 300$ comission\n'
@@ -94,21 +110,21 @@ def main():
 
             elif choose == '2':
                 amount = input('How much would you like to deposit?\n')
-                deposit(id_num, amount, pass_dict, bal_dict)
-                print('now your balance is {}'.format(check_balance
+                user.set_deposit(id_num, amount, pass_dict, bal_dict)
+                print('now your balance is {}'.format(user.get_balance
                                                       (id_num, bal_dict)))
                 break
             elif choose == '3':
                 amount = input('How much would you like to withdraw?\n')
-                withdraw(id_num, amount, pass_dict, bal_dict)
+                user.set_withdraw(id_num, amount, pass_dict, bal_dict)
                 print('now your balance is {}'.format
-                      (check_balance(id_num, bal_dict)))
+                      (user.get_balance(id_num, bal_dict)))
                 break
             elif choose == '4':
                 new_password = '0'
                 while len(new_password) != 4:
                     new_password = input('please enter 4 digit password\n')
-                change_password(new_password, id_num, pass_dict, bal_dict)
+                user.set_change_password(new_password, id_num, pass_dict, bal_dict)
                 print('password successfully changed')
                 break
             else:
