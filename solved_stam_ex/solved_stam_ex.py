dcdc6b34d8d33dfdd4df2c8f3a8b28bcd5fec086
lielya <liel.nana1@gmail.com>
2020-03-31 16:37:38 +0300

    use consts

--- solved_stam_ex/solved_stam_ex.py
+++ solved_stam_ex/solved_stam_ex.py
@@ -1,36 +1,26 @@
-"""A program that receive a file name, and add specific words to content.
-
-Add your first name to the beginning of each line,
-and your last name to the end of each line.
-
-"""
+"""A program that receive a file name, and add specific words to content. """
 
 import sys
 
-PATH_FILE_WITH_CONTENT = 1
+PATH_CONTENT_FILE = 1
 PATH_EMPTY_FILE = 2
+FIRST_NAME = 'liel'
+LAST_NAME = 'yaakobov'
 
 
 def main():
-    firs_name = 'liel'
-    last_name = 'yaakobov'
-
     try:
-        with open(sys.argv[PATH_FILE_WITH_CONTENT], 'r') \
-                as pointer_file_with_content:
-            with open(sys.argv[PATH_EMPTY_FILE], 'w') as pointer_empty_file:
-
-                for line in pointer_file_with_content:
-                    pointer_empty_file.write(f"{firs_name} {line.rstrip()}"
-                                             f" {last_name}\n")
+        with open(sys.argv[PATH_CONTENT_FILE], 'r') as content_file:
+            with open(sys.argv[PATH_EMPTY_FILE], 'w') as empty_file:
+                for line in content_file:
+                    empty_file.write(f"{FIRST_NAME} {line.rstrip()}"
+                                     f" {LAST_NAME}\n")
 
     except FileNotFoundError:
         print("The path you entered is not correct. Please check it")
-
     except Exception as e:
         print(e)
 
 
 if __name__ == '__main__':
     main()
-
