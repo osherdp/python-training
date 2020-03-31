131d9c6fc9bb95509dac644898eb3021f2ff9c20
guyha <guyharamati10@gmail.com>
2020-03-31 20:53:53 +0300

    flake8 fixed

--- basic/stam/Guy/stam_exe.py
+++ basic/stam/Guy/stam_exe.py
@@ -12,10 +12,11 @@ Then you should save the newly created file to a new location.
 """
 
 
-def add_my_name(filename,destfile, firstname, lastname):
+def add_my_name(filename, destfile, firstname, lastname):
     """
     this function receives a source file name, and first+last name
-    creates a new file with first name at the beginning of each line and last name
+    creates a new file with first name at the beginning of each line
+    and last name
     at the end of each line.
     this new file location will be according to input destfile
     """
@@ -23,7 +24,8 @@ def add_my_name(filename,destfile, firstname, lastname):
     fd_out = open(destfile, 'w')
     for line in fd_in:
         """remove \n from each line"""
-        fd_out.write(firstname + ' ' + line[0:len(line) - 1:] + ' ' + lastname + "\n")
+        fd_out.write(firstname + ' ' + line[0:len(line) - 1:] + ' ' +
+                     lastname + "\n")
     fd_out.close()
     fd_in.close()
 
@@ -33,8 +35,8 @@ def main():
     lastname = 'Haramati'
     filename = r"C:\guy\python\alice.txt"
     destfile = r"C:\guy\python\add_my_name.txt"
-    print 'My name is {} {}'.format(firstname, lastname)
-    add_my_name(filename,destfile, firstname, lastname)
+    print('My name is {} {}'.format(firstname, lastname))
+    add_my_name(filename, destfile, firstname, lastname)
 
 
 if __name__ == '__main__':
