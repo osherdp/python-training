6a08a7cb1e82f816045d67f92cf1b6c3c0bf6ee9
michaelstopel <62503333+michaelstopel@users.noreply.github.com>
2020-03-31 13:01:00 +0300

    ATM_FINAL

--- basic/atm/atm.py
+++ basic/atm/atm.py
@@ -1,73 +1,96 @@
-#def  dictionary(old):
-   # text= old.read()
-  # list_1 = text.split('/n')
-   # update_id = id(list_1[0])
-   #  return
-def deleteContent(pfile):
 
+def delete_old_file(pfile):
+    """delete the old file of ATM data
+    """
     pfile.seek(0)
     pfile.truncate()
     pfile.seek(0) # I believe this seek is redundant
     return pfile
+def file_to_list(file):
+    """convert file to list
+        """
+    text = file.read()
+    list_1 = text.split('\n')
+    i = 0
+    list_2 = []
+    while i < len(list_1):
+        list_2 = list_2 + list_1[i].split(',')
+        i = i + 1
+    return list_2
+def dictionary_id_password(list):
+    """create dictionary id:password from list
+                """
+    i = 0
+    password_dct = {list[i]: list[i + 1] for i in range(0, len(list) - 1, 3)}
+    return password_dct
+def dictionary_id_money(list):
+    """create dictionary id:money from list
+            """
+    i = 0
+    money_dct = {list[i]: list[i + 2] for i in range(0, len(list) - 1, 3)}
+    return money_dct
+def get_password(id,password):
+    """check and change the password
+            """
+    inncorrect_flag = 1
+    while (inncorrect_flag == 1):
+        if input('input your password:\n') == password.get(id):
+            inncorrect_flag = 0
+            if input('Do you want your password?:\n') == 'yes':
+                password[id] = input('new password?:\n')
+        else:
+            print('incorrect password')
+            inncorrect_flag = 1
 
+    return  password
+def Cash_withdrawal(money):
+    """check cash withdrawal
+               """
+    if input('Do you want to do Cash withdrawal?:\n') == 'yes':
+        money[id] = float(money[id]) - float(input('how much Cash withdrawal:\n'))
+        print("your balance is", money.get(id))
+    return money
+def Cash_deposit(money):
+    """check cash deposit
+                  """
+    if input('Do you want to do Cash deposit?:\n') == 'yes':
+        money[id] = float(money[id]) + float(input('Cash deposit:\n'))
+        print("your balance is", money.get(id))
+    return money
+def dictionary_to_file(password,money,file):
+    """change dictionary back to file
+                  """
+    l = []
+    [l.extend([k, v]) for k, v in password.items()]
+    l_1 = l[0:len(l):2]
 
+    l_2 = l[1:len(l):2]
+
+    j = []
+    [j.extend([k, v]) for k, v in money.items()]
+    j = j[1:len(l):2]
+    i = 0
+    while i < len(l_1):
+        l_1[i] = str(l_1[i]) + ',' +str(l_2[i]) + ',' + str(j[i])
+        i += 1
+    text = '\n'.join(l_1)
+    file = file.write(text)
+    return file
+
+    if __name__ == '__main__':
+        main()
 file = open(r'C:\Users\97254\Documents\GitHub\python-training\basic\atm\file.txt', 'r')
-
-text= file.read()
-list_1 = text.split('\n')
-
-i = 0
-list_2 = []
-while i< len(list_1):
-    list_2 = list_2 + list_1[i].split(',')
-    i = i+1
-i = 0
-password_dct = {list_2[i]: list_2[i + 1] for i in range(0, len(list_2)-1, 3)}
-money_dct = {list_2[i]: list_2[i + 2] for i in range(0, len(list_2)-1, 3)}
-
-
+list_file = file_to_list(file)
+password = dictionary_id_password(list_file)
+money = dictionary_id_money(list_file)
 id = input('input your id:\n')
-#password = password_dct.get(id)
-money = money_dct.get(id)
+if int(id) != -1:
+    update_password = get_password(id,password)
+    print("your balance is",money.get(id))
+    update_money = Cash_withdrawal(money)
+    update_money = Cash_deposit(money)
+    file= delete_old_file(open(r'C:\Users\97254\Documents\GitHub\python-training\basic\atm\file.txt', 'a'))
+    file = dictionary_to_file(update_password,update_money,file)
+else:
+    print('close the ATM')
 
-count = 0
-inncorrect_flag = 1
-while(inncorrect_flag == 1):
-    if input('input your password:\n') == password_dct.get(id) :
-         inncorrect_flag = 0
-         if input('Do you want your password?:\n') == 'yes':
-             password_dct[id] = input('new password?:\n')
-         print("your balance is",money_dct.get(id))
-         if input('Do you want to do Cash withdrawal?:\n') == 'yes':
-             money_dct[id] = float(money_dct[id])- float(input('how much Cash withdrawal:\n'))
-             print("your balance is", money_dct.get(id))
-         if input('Do you want to do Cash withdrawal?:\n') == 'yes':
-             money_dct[id] = float(money_dct[id]) + float(input('Cash deposit:\n'))
-             print("your balance is", money_dct.get(id))
-
-    else:
-         print('incorrect password')
-         inncorrect_flag = 1
-         count = count + 1
-         if (count == 3):
-             print('You entered the wrong password three times')
-             break
-
-#I = password_dct.items()
-l=[]
-[l.extend([k,v]) for k,v in password_dct.items()]
-l_1=l[0:len(l):2]
-
-l_2=l[1:len(l):2]
-
-j=[]
-[j.extend([k,v]) for k,v in money_dct.items()]
-j=j[1:len(l):2]
-while i < len(l_1):
-    l_1[i] = l_1[i] + ',' + l_2[i] + ',' + j[i]
-    i += 1
-text_2 = '\n'.join(l_1)
-
-file= deleteContent(open(r'C:\Users\97254\Documents\GitHub\python-training\basic\atm\file.txt', 'a'))
-file = file.write(text_2)
-print(text_2)
