eafc74c6d55a53b41fa28c0b83ca3c5e2230468e
Yonatan Evantal <yonatan.evantal@gmail.com>
2020-03-31 13:38:24 +0300

    Different methods for most inner parentheses

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -10,14 +10,56 @@ def _remove_spaces(expression: str) -> str:
     return ''.join(expression.split())
 
 
-# def _innermost_parentheses(text: str) -> str:
-#     """Return the text inside the innermost parenthesis."""
-#     if all(['(' not in text, ')' not in text]):
-#         return text
-#
-#     open_parentheses = text.index('(')
-#     close_parentheses = text.index(')')
-#     return _innermost_parentheses(text[open_parentheses + 1:close_parentheses])
+def _innermost_parentheses(text: list) -> list:
+    """Return the text inside the innermost parenthesis."""
+    if all(['(' not in text, ')' not in text]):
+        return text
+
+    print(text)
+
+    open_paren = text.index('(')
+    close_paren = text.index(')')
+
+    print(open_paren, close_paren)
+    print(text)
+
+    return _innermost_parentheses(text[open_paren + 1:close_paren])
+
+
+def max_depth(s):
+    current_max = 0
+    max = 0
+    n = len(s)
+
+    # Traverse the input string
+    for i in range(n):
+        if s[i] == '(':
+            current_max += 1
+
+            if current_max > max:
+                max = current_max
+        elif s[i] == ')':
+            if current_max > 0:
+                current_max -= 1
+            else:
+                return -1
+
+    # finally check for unbalanced string
+    if current_max != 0:
+        return -1
+
+    return max
+
+
+def parenthetic_contents(string):
+    """Generate parenthesized contents in string as pairs (level, contents)."""
+    stack = []
+    for i, c in enumerate(string):
+        if c == '(':
+            stack.append(i)
+        elif c == ')' and stack:
+            start = stack.pop()
+            yield len(stack), string[start + 1: i]
 
 
 class Parser:
@@ -26,13 +68,14 @@ class Parser:
     def __init__(self, expression: str):
         self.expression = _remove_spaces(expression)
 
-    def _split(self):
+    def split(self):
         supported_operators = ','.join(operators.keys())
         negative_positive_ints = r'[+-]?\d+'
         negative_positive_floats = negative_positive_ints + r'\.?\d+'
 
         number_or_symbol = re.compile(
-            rf'([{supported_operators}]|{negative_positive_floats}|{negative_positive_ints})')
+            rf'([{supported_operators}]|{negative_positive_floats}'
+            rf'|{negative_positive_ints})')
 
         return re.findall(number_or_symbol, self.expression)
 
@@ -40,4 +83,7 @@ class Parser:
 # source = '((81 * 6) /42+ (3-1))'
 source = '((81 * 6) /42+ (3.5-1))'
 c = Parser(source)
-print(c._split())
+print(c.split())
+# print(_innermost_parentheses(c.split()))
+print(max_depth(c.split()))
+print(list(parenthetic_contents(source)))
