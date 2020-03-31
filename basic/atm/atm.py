c2e918b8da594351635401a2e8ae280128b55080
galyammaya <galyammaya@gmail.com>
2020-03-31 19:39:07 +0300

    atm fixed 2

--- basic/atm/atm.py
+++ basic/atm/atm.py
@@ -5,8 +5,12 @@ class ATM:
     ID_INDEX = 3
     CUSTOMER_LINE_FORMAT = '{};{};{};{}\n'
 
-    def get_customers(self, file_path):
-        with open(file_path, 'r') as file:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.customers = self.get_customers()
+
+    def get_customers(self):
+        with open(self.file_path, 'r') as file:
             customers = {}
             for line in file.readlines():
                 data = line.strip().split(';')
@@ -16,35 +20,36 @@ class ATM:
                                                           self.BALANCE_INDEX])]
         return customers
 
-    def check_balance(self, customers, ID):
-        print('Your balance is: {}'.format(customers[ID][self.BALANCE_INDEX]))
+    def check_balance(self, ID):
+        print('Your balance is: {}'.format(
+            self.customers[ID][self.BALANCE_INDEX]))
 
-    def deposit(self, customers, ID):
+    def deposit(self, ID):
         try:
             amount = int(input('Enter amount to deposit: '))
             if amount > 0:
-                customers[ID][self.BALANCE_INDEX] += amount
+                self.customers[ID][self.BALANCE_INDEX] += amount
             else:
                 print('Please enter a positive number')
         except ValueError:
             print('Please enter an integer')
 
-    def withdraw(self, customers, ID):
+    def withdraw(self, ID):
         try:
             amount = int(input('Enter amount to withdraw: '))
             if amount > 0:
-                customers[ID][self.BALANCE_INDEX] -= amount
+                self.customers[ID][self.BALANCE_INDEX] -= amount
             else:
                 print('Please enter a positive number')
         except ValueError:
             print('Please enter an integer')
 
-    def change_password(self, customers, ID):
+    def change_password(self, ID):
         tries = 0
         while tries < 3:
             password = input('Enter old password: ')
-            if password == customers[ID][self.PASSWORD_INDEX]:
-                customers[ID][self.PASSWORD_INDEX] = input(
+            if password == self.customers[ID][self.PASSWORD_INDEX]:
+                self.customers[ID][self.PASSWORD_INDEX] = input(
                     'Enter new password: ')
                 break
             else:
@@ -53,11 +58,11 @@ class ATM:
         if tries == 3:
             print('Exceeded number of tries allowed')
 
-    def update_file(self, customers, file_path):
-        with open(file_path, 'w') as customers_file:
-            for key in customers:
+    def update_file(self):
+        with open(self.file_path, 'w') as customers_file:
+            for key in self.customers:
                 customers_file.writelines(self.CUSTOMER_LINE_FORMAT.format(
-                    customers[key][self.NAME_INDEX],
-                    customers[key][self.PASSWORD_INDEX],
-                    customers[key][self.BALANCE_INDEX],
+                    self.customers[key][self.NAME_INDEX],
+                    self.customers[key][self.PASSWORD_INDEX],
+                    self.customers[key][self.BALANCE_INDEX],
                     key))
--- basic/atm/framework.py
+++ basic/atm/framework.py
@@ -3,10 +3,10 @@ from atm import ATM
 
 CUSTOMERS_FILE_LOCATION = 1
 NAME_INDEX = 0
-MENU_CHECK = 1
-MENU_DEPOSIT = 2
-MENU_WITHDRAW = 3
-MENU_CHANGE_PASS = 4
+MENU_CHECK = '1'
+MENU_DEPOSIT = '2'
+MENU_WITHDRAW = '3'
+MENU_CHANGE_PASS = '4'
 WELCOME_MESSAGE = 'Welcome to the ATM! \n ' \
                   'To exit enter -1, ' \
                   'otherwise please enter your customer ID:'
@@ -17,83 +17,22 @@ MENU_MESSAGE = 'To check your balance, press 1 \n' \
                'To change your password, press 4 \n '
 
 
-# def get_customers(file):
-#     customers = {}
-#     for line in file.readlines():
-#         data = line.strip().split(';')
-#         customers[data[ID_INDEX]] = [data[NAME_INDEX],
-#                                      data[PASSWORD_INDEX],
-#                                      int(data[BALANCE_INDEX])]
-#     return customers
-
-
-# def check_balance(customers, ID):
-#     print('Your balance is: {}'.format(customers[ID][BALANCE_INDEX]))
-
-
-# def deposit(customers, ID):
-#     try:
-#         amount = int(input('Enter amount to deposit: '))
-#         if amount > 0:
-#             customers[ID][BALANCE_INDEX] += amount
-#         else:
-#             print('Please enter a positive number')
-#     except ValueError:
-#         print('Please enter an integer')
-
-
-# def withdraw(customers, ID):
-#     try:
-#         amount = int(input('Enter amount to withdraw: '))
-#         if amount > 0:
-#             customers[ID][BALANCE_INDEX] -= amount
-#         else:
-#             print('Please enter a positive number')
-#     except ValueError:
-#         print('Please enter an integer')
-
-
-# def change_password(customers, ID):
-#     tries = 0
-#     while tries < 3:
-#         password = input('Enter old password: ')
-#         if password == customers[ID][PASSWORD_INDEX]:
-#             customers[ID][PASSWORD_INDEX] = input('Enter new password: ')
-#             break
-#         else:
-#             print('Incorrect password, try again')
-#             tries += 1
-#     if tries == 3:
-#         print('Exceeded number of tries allowed')
-
-
-# def update_file(customers):
-#     with open(customers_file_path, 'w') as customers_file:
-#         for key in customers:
-#             customers_file.writelines(
-#                 CUSTOMER_LINE_FORMAT.format(customers[key][NAME_INDEX],
-#                                             customers[key][PASSWORD_INDEX],
-#                                             customers[key][BALANCE_INDEX],
-#                                             key))
-
-
 def main():
-    atm = ATM()
-    customers = atm.get_customers(customers_file_path)
+    atm = ATM(customers_file_path)
     flag = True
     while flag:
         cust_ID = input(WELCOME_MESSAGE)
-        if cust_ID in customers.keys():
-            print(HELLO_MESSAGE.format(customers[cust_ID][NAME_INDEX]))
+        if cust_ID in atm.customers.keys():
+            print(HELLO_MESSAGE.format(atm.customers[cust_ID][NAME_INDEX]))
             user_input = input(MENU_MESSAGE)
             if user_input == MENU_CHECK:
-                atm.check_balance(customers, cust_ID)
+                atm.check_balance(cust_ID)
             elif user_input == MENU_DEPOSIT:
-                atm.deposit(customers, cust_ID)
+                atm.deposit(cust_ID)
             elif user_input == MENU_WITHDRAW:
-                atm.withdraw(customers, cust_ID)
+                atm.withdraw(cust_ID)
             elif user_input == MENU_CHANGE_PASS:
-                atm.change_password(customers, cust_ID)
+                atm.change_password(cust_ID)
             else:
                 print('Invalid choice')
         elif cust_ID == '-1':
@@ -101,7 +40,7 @@ def main():
             flag = False
         else:
             print('ID does not exist')
-    atm.update_file(customers, customers_file_path)
+    atm.update_file()
 
 
 if __name__ == '__main__':
