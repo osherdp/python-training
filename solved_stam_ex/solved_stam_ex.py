33e41c8bd3ea6981d7c7f500bd5fd5b23c179188
lielya <liel.nana1@gmail.com>
2020-03-31 15:20:14 +0300

    add doc string indicative names use fstring not using magic strings add except

--- solved_stam_ex/solved_stam_ex.py
+++ solved_stam_ex/solved_stam_ex.py
@@ -1,25 +1,29 @@
+"""A program that receive a file name, and add specific words to content.
+
+Add your first name to the beginning of each line,
+and your last name to the end of each line.
+
+"""
+
 import sys
 
-FILE1 = 1
-FILE2 = 2
+PATH_FILE_WITH_CONTENT = 1
+PATH_EMPTY_FILE = 2
 
 
 def main():
-    try:
-        with open(sys.argv[FILE1], 'r') as f1:
-            with open(sys.argv[FILE2], 'w') as f2:
-                f_content = f1.read().split('\n')
-                print(f_content)
-                new_content = (list(map(
-                    lambda x: 'liel' + ' ' + x + ' ' + 'yaakobov',
-                    f_content)))
-                print(new_content)
-                for i in new_content:
-                    # print(i)
-                    f2.writelines("{}\n".format(i))
+    firs_name = 'liel'
+    last_name = 'yaakobov'
 
-        with open(sys.argv[FILE2], 'r') as f:
-            print(f.read())
+    try:
+        with open(sys.argv[PATH_FILE_WITH_CONTENT], 'r') as pointer_file_with_content:
+            with open(sys.argv[PATH_EMPTY_FILE], 'w') as pointer_empty_file:
+
+                for line in pointer_file_with_content:
+                    pointer_empty_file.write(f"{firs_name} {line.rstrip()} {last_name}\n")
+
+    except FileNotFoundError:
+        print("The path you entered is not correct. Please check it")
 
     except Exception as e:
         print(e)
@@ -27,3 +31,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
