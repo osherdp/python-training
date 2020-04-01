44ea99b9873ae1d76fbde98ffd190eda974fa8a3
AdiKadarLevi <62499966+AdiKadarLevi@users.noreply.github.com>
2020-04-01 13:59:49 +0300

    Update ATM.py

--- ATM.py
+++ ATM.py
@@ -2,12 +2,17 @@ __author__ = 'Adi Kadar Levi'
 
 BANK_DATA_PATH = r'C:\Users\Elior Mor Yosef\Documents\GitHub' \
       r'\python-training\python-training\bank.txt'
+BALANCE_CHOICE = '1'
+DEPOSIT_CHOICE = '2'
+WITHDRAW_CHOICE = '3'
+CHANGE_PASSWORD_CHOICE = '4'
+EXIT_CHOICE = '-1'
 
 
 class ATM:
 
     def __init__(self):
-        self.id_num = 0
+        self.id_num = '0'
         self.password = 0
         self.balance = 0
         self.choice = '0'
@@ -21,7 +26,7 @@ class ATM:
                 passwords = words[1]
                 balances = words[2]
                 self.pass_dict[id_numbers] = passwords
-                self.balance_dict[words[0]] = balances
+                self.balance_dict[id_numbers] = float(balances)
 
     def select(self):
         self.choice = input('What would you like to do today?\n'
@@ -30,31 +35,59 @@ class ATM:
                             'To withdraw money press 3\n'
                             'To change password press 4\n'
                             'for Exit press -1\n')
-        return self.choice
+        if self.choice == BALANCE_CHOICE:
+            balance = self.balance_dict[self.id_num]
+            print('your balance is {}'.format(balance))
+            print('Thank you for using our services\n'
+                  'For this action '
+                  'you will be charged for 300$ commission\n'
+                  'Bye Bye!')
 
-    @staticmethod
-    def check_password(user_id, input_password, pass_data):
-        while pass_data[user_id] != input_password:
+        elif self.choice == DEPOSIT_CHOICE:
+            amount = float(input('How much would you like to deposit?\n'))
+            self.deposit(amount)
+            self.update_data()
+            balance = self.balance_dict[self.id_num]
+            print('now your balance is {}'.format(balance))
+
+        elif self.choice == WITHDRAW_CHOICE:
+            amount = float(input('How much would you like to withdraw?\n'))
+            self.withdraw(amount)
+            self.update_data()
+            balance = self.balance_dict[self.id_num]
+            print('now your balance is {}'.format(balance))
+
+        elif self.choice == CHANGE_PASSWORD_CHOICE:
+            new_password = '0'
+            while len(new_password) != 4:
+                new_password = input('please enter 4 digit password\n')
+            self.change_password(new_password)
+            self.update_data()
+            print('password successfully changed')
+
+        elif self.choice == EXIT_CHOICE:
+            print('Bye!')
+
+        else:
+            print('invalid input')
+
+    def check_password(self, input_password):
+        while self.pass_dict[format(self.id_num)] != input_password:
             print('password incorrect\n')
             input_password = input('please enter your password\n')
 
-    def get_balance(self, id_num, balance_data):
-        self.balance = balance_data[id_num]
-        return float(self.balance)
+    def withdraw(self, amount_to_withdraw):
+        self.balance_dict[self.id_num] = self.balance_dict[
+                                             self.id_num] - \
+                                         amount_to_withdraw
 
-    @staticmethod
-    def withdraw(user_id, amount_to_withdraw, balance_dict):
-        balance_dict[user_id] = float(balance_dict
-                                      [user_id]) - float(amount_to_withdraw)
+    def deposit(self, amount_to_deposit):
+        self.balance_dict[self.id_num] = self.balance_dict[
+                                             self.id_num] +\
+                                         amount_to_deposit
 
-    @staticmethod
-    def deposit(user_id, amount_to_deposit, balance_dict):
-        balance_dict[user_id] = float(amount_to_deposit) + float(balance_dict
-                                                                 [user_id])
-
-    @staticmethod
-    def change_password(new_password, user_id, pass_dict):
-        pass_dict[user_id] = new_password
+    def change_password(self, new_password):
+        self.pass_dict[self.id_num] = new_password
 
     def update_data(self):
         with open(BANK_DATA_PATH, 'w') as new_bank:
@@ -70,50 +103,9 @@ def main():
     if id_num in user.pass_dict.keys():
         user.id_num = id_num
         user.password = input('please enter your password\n')
-        user.check_password(id_num, user.password, user.pass_dict)
+        user.check_password(user.password)
+        user.select()
 
-        balance_choice = '1'
-        deposit_choice = '2'
-        withdraw_choice = '3'
-        change_pass_choice = '4'
-        exit_choice = '-1'
-        user.choice = user.select()
-        if user.choice == balance_choice:
-            balance = user.get_balance(id_num, user.balance_dict)
-            print('your balance is {}'.format(balance))
-            print('Thank you for using our sevices\n'
-                  'For this action '
-                  'you will be charged for 300$ comission\n'
-                  'Bye Bye!')
-
-        elif user.choice == deposit_choice:
-            amount = input('How much would you like to deposit?\n')
-            user.deposit(user.id_num, amount, user.balance_dict)
-            user.update_data()
-            print('now your balance is {}'.format(user.get_balance
-                                                  (id_num,
-                                                   user.balance_dict)))
-
-        elif user.choice == withdraw_choice:
-            amount = input('How much would you like to withdraw?\n')
-            user.withdraw(user.id_num, amount, user.balance_dict)
-            user.update_data()
-            print('now your balance is {}'.format
-                  (user.get_balance(id_num, user.balance_dict)))
-
-        elif user.choice == change_pass_choice:
-            new_password = '0'
-            while len(new_password) != 4:
-                new_password = input('please enter 4 digit password\n')
-            user.change_password(new_password, user.id_num, user.pass_dict)
-            user.update_data()
-            print('password successfully changed')
-
-        elif user.choice == exit_choice:
-            print('Bye!')
-
-        else:
-            print('invalid input')
     else:
         print('User Not Found')
 
