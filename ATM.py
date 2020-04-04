aaa1904ba6ed97604a416d987dd20cf46af31822
keren kay <keren.kay.2001@gmail.com>
2020-04-04 20:47:02 +0300

    commit ATM

--- ATM.py
+++ ATM.py
@@ -3,35 +3,57 @@ import sys
 
 
 def file_to_dic(filename):
+    """
+
+    :arg
+        filename(.txt file) - parameter
+
+    :return:
+        a dictionary, key=id and value=[password, balance]
+
+    """
     atm = {}
     with open(filename, 'r') as file:
         for line in file:
-            details = line.split(' ')
+            details = line.strip().split(' ')
             atm[details[0]] = details[1:]
 
     return atm
 
 
 def atm_machine(id, atm_dic):
-    if atm_dic.get(ID):
-        action = input("enter action- check the balance\\"
-                       "cash withdrawal\\cash deposit"
-                       "\\change password")
+    """
 
-        if action == 'check the balance':
-            print(f"your balance is: {atm_dic[id][2]}")
+    Args:
+        id (str)- first parameter
+        atm_dic (dic)- second parameter
 
-        elif action == 'cash withdrawal':
-            money = input("enter the amount you want to withdraw")
-            atm_dic[id][2] = atm_dic[id][2] - money
+    :return:
+        A dictionary with the updated values
+        after performing the desired action
 
-        elif action == 'cash deposit':
-            money = input("enter the amount you want to deposit")
-            atm_dic[id][2] = atm_dic[id][2] + money
+    """
+    if atm_dic.get(id):
+        action = input("enter number of action:\n"
+                       "1- for check the balance\n"
+                       "2- for cash withdrawal\n"
+                       "3- for cash deposit\n"
+                       "4- for change password\n")
 
-        elif action == 'change password':
-            password = input('enter new password')
-            atm_dic[id][1] = password
+        if action == '1':
+            print(f"your balance is: {atm_dic[id][1]}")
+
+        elif action == '2':
+            money = input("enter the amount you want to withdraw\n")
+            atm_dic[id][1] = str(int(atm_dic[id][1]) - int(money))
+
+        elif action == '3':
+            money = input("enter the amount you want to deposit\n")
+            atm_dic[id][1] = str(int(atm_dic[id][1]) + int(money))
+
+        elif action == '4':
+            password = input('enter new password\n')
+            atm_dic[id][0] = password
 
         else:
             print('action not detected')
@@ -41,12 +63,29 @@ def atm_machine(id, atm_dic):
 
 
 def main():
+    """
+
+    :arg
+        id(int) - 9 digits ID (example: 322781145)
+
+    :return:
+        will do the desired ATM operation accordig to an
+        ID that has been accepted until receive -1 as an ID.
+        all the chages will be saved to the ATM.
+
+    """
     if os.path.isfile(sys.argv[1]):
         atm_dic = file_to_dic(sys.argv[1])
-        id = input('please enter your ID')
-        while id != -1:
+        id = input('please enter your ID\n')
+
+        while id != '-1':
             atm_machine(id, atm_dic)
-            id = input('please enter your ID')
+            id = input('please enter your ID\n')
+
+        with open(sys.argv[1],'w') as newfile:
+            for id in atm_dic:
+                newfile.writelines(f"{id} {atm_dic[id][0]} {atm_dic[id][1]}\n")
+
     else:
         print('file not found')
 
