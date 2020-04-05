c85c953dd293b2e5be4883f152270f4a1bd5bf31
Karoline123121 <keren084@gmail.com>
2020-04-05 14:17:30 +0300

    applied necessary changes

--- advanced/matrix/matrix.py
+++ advanced/matrix/matrix.py
@@ -5,39 +5,56 @@ class Matrix:
     """A class used to represent an immutable n*n matrix.
 
         Attributes:
-            __matrix (tuple): a tuple of tuples representing a matrix.
+            matrix_rows (tuple): a tuple of tuples representing a matrix.
+
+        Raises:
+            TypeError: self is not a tuple of tuples.
+            ValueError: self doesn't have an equal number of rows and columns.
     """
     def __init__(self, matrix_tuples):
         """Initialize instance of matrix and check for validity."""
-        self.__matrix = matrix_tuples
+        self.matrix_rows = matrix_tuples
         self.is_good_matrix()
 
     def _check_even(self):
-        """Check that matrix has equal number of rows and columns."""
-        for mat_tuple in self.__matrix:
-            if len(mat_tuple) != len(self.__matrix):
-                raise ValueError("The matrix is not n*n")
+        """Check that matrix has equal number of rows and columns.
+
+        Raises:
+            ValueError: self doesn't have an equal number of rows and columns.
+        """
+        for mat_tuple in self.matrix_rows:
+            if len(mat_tuple) != len(self.matrix_rows):
+                raise ValueError("The matrix is not n*n.")
 
     def _check_tuple(self):
-        """Check that matrix is made of a tuple of tuples."""
-        if isinstance(self.__matrix, tuple):
-            if not all(isinstance(mat_tuple, tuple)
-                       for mat_tuple in self.__matrix):
+        """Check that matrix is made of a tuple of tuples.
 
-                raise TypeError("Object is not a tuple's matrix.")
+        Raises:
+            TypeError: self is not a tuple of tuples.
+        """
+        if isinstance(self.matrix_rows, tuple):
+            if not all(isinstance(matrix_row, tuple)
+                       for matrix_row in self.matrix_rows):
+
+                raise TypeError("Object is not a tuple of tuples.")
 
         else:
-            raise TypeError("Object is not a tuple's matrix.")
+            raise TypeError("Object is not a tuple of tuples.")
 
     def is_good_matrix(self):
-        """Check that matrix given is valid."""
+        """Check that matrix given is valid.
+
+        Raises:
+            TypeError: self is not a tuple of tuples.
+            ValueError: self doesn't have an equal number of rows and columns.
+        """
         self._check_tuple()
         self._check_even()
 
     @property
     def tuples(self):
         """Return the matrix."""
-        return self.__matrix
+        return self.matrix_rows
 
     @classmethod
     def unity(cls, size):
@@ -47,7 +64,7 @@ class Matrix:
             size (int): the number of rows and columns for new matrix.
 
         Returns:
-            tuple: A New matrix where main diagonal is 1, the rest 0.
+            Matrix: A New matrix where main diagonal is 1, the rest 0.
         """
         u_matrix = tuple(tuple(1 if col == row else 0 for col in range(size))
                          for row in range(size))
@@ -62,22 +79,27 @@ class Matrix:
             size: The size of the new matrix.
 
         Returns:
-            tuple: Tuple of tuples filled with ones.
+            Matrix: Tuple of tuples filled with ones.
         """
         ones_mat = (((1,) * size,) * size)
         return cls(ones_mat)
 
-    def _multiplication(self, other_matrix):
+    def _multiply_matrices(self, other_matrix):
         """Calculate the multiplication of 2 matrices.
 
             Args:
-                other_matrix (tuple): A matrix to multiply with.
+                other_matrix (Matrix): A matrix to multiply with.
 
             Returns:
                 tuple: Result matrix of the multiplication between 2 matrices.
+
+            Raises:
+                TypeError: other is not a matrix.
+                ValueError: other matrix is not the same size as self.
         """
+        self._check_valid_matrix(other_matrix)
         result = tuple(tuple(sum(n_a * n_b for n_a, n_b in zip(a_row, b_col))
-                       for b_col in zip(*other_matrix))
+                       for b_col in zip(*other_matrix.tuples))
                        for a_row in self)
         return self.__class__(result)
 
@@ -89,7 +111,6 @@ class Matrix:
 
         Returns:
             Matrix : The new matrix multiplied by scalar.
-
         """
         return self.__class__(tuple(tuple(num * scalar for num in each_tuple)
                               for each_tuple in self))
@@ -102,20 +123,27 @@ class Matrix:
 
         Returns:
             Matrix: The result matrix.
+
+        Raises:
+            TypeError: other is not a scalar(int/float) or a matrix.
+            ValueError: other matrix is not the same size as self.
         """
-        try:
-            self._check_valid_matrix(other)
-            return self._multiplication(other.tuples)
+        if isinstance(other, Matrix):
+            return self._multiply_matrices(other)
 
-        except TypeError:
-            if isinstance(other, int) or isinstance(other, float):
-                return self._multiply_by_scalar(other)
+        elif isinstance(other, int) or isinstance(other, float):
+            return self._multiply_by_scalar(other)
 
-            else:
-                raise TypeError("Can only multiply numbers and matrices.")
+        else:
+            raise TypeError("Can only multiply numbers and matrices.")
 
     def _check_valid_matrix(self, other):
-        """Check that 'other' is a matrix of the same size as self."""
+        """Check that 'other' is a matrix of the same size as self.
+
+        Raises:
+            TypeError: other is not of Matrix type.
+            ValueError: other matrix isn't the same size as self matrix.
+        """
         if isinstance(other, Matrix):
             if len(other.tuples) != len(self.tuples):
                 raise ValueError("Cannot operate on different sized matrices.")
@@ -130,10 +158,13 @@ class Matrix:
     def __truediv__(self, other):
         """Return new matrix divided by scalar.
         Args:
-            other (float/int): Scalar to divide by
+            other (float/int): Scalar to divide by.
 
         Returns:
             Matrix: The matrix divided by the scalar.
+
+        Raises:
+            TypeError: other is not a scalar(int/float).
         """
         if isinstance(other, int) or isinstance(other, float):
             return self * (1 / other)
@@ -142,15 +173,26 @@ class Matrix:
             raise TypeError("'other' argument must be a number.")
 
     def __add__(self, other):
-        """Return result of 2 matrices addition."""
+        """Return result of 2 matrices addition.
+
+        Raises:
+            TypeError: other is not of Matrix type.
+            ValueError: other matrix isn't the same size as self matrix.
+        """
         self._check_valid_matrix(other)
-        add_result = (tuple(x + y for x, y in zip(tuple1, tuple2))
-                      for tuple1, tuple2 in zip(self, other))
+        add_result = (tuple(matrix1_col + matrix2_col
+                            for matrix1_col, matrix2_col in zip(matrix1_row, matrix2_row))
+                      for matrix1_row, matrix2_row in zip(self, other))
 
         return self.__class__(tuple(add_result))
 
     def __sub__(self, other):
-        """Return result of 2 matrices subtraction."""
+        """Return result of 2 matrices subtraction.
+
+        Raises:
+            TypeError: other is not of Matrix type.
+            ValueError: other matrix isn't the same size as self matrix.
+        """
         return self + (-1 * other)
 
     def __iter__(self):
--- advanced/matrix/test_matrix.py
+++ advanced/matrix/test_matrix.py
@@ -14,7 +14,7 @@ class TestMatrix(unittest.TestCase):
 
     def test_tuples_property(self):
         """Test tuples property of matrix."""
-        self.assertEqual(self.mat_a.tuples, ((1, 2), (3, 4)))
+        self.assertEqual(self.mat_a.matrix_rows, ((1, 2), (3, 4)))
 
     def test_type_validation(self):
         """Test that only a tuple of tuples is allowed in Matrix."""
@@ -28,38 +28,43 @@ class TestMatrix(unittest.TestCase):
 
     def test_unity_2_dimensional(self):
         """Test unity method on 2 by 2 matrix."""
-        self.assertEqual(Matrix.unity(2), Matrix(((1, 0), (0, 1))))
+        self.assertEqual(Matrix.unity(2).matrix_rows, ((1, 0), (0, 1)))
 
     def test_unity_3_dimensional(self):
         """Test unity method on 3 by 3 matrix."""
-        self.assertEqual(Matrix.unity(3),
-                         Matrix(((1, 0, 0), (0, 1, 0), (0, 0, 1))))
+        self.assertEqual(Matrix.unity(3).matrix_rows,
+                         ((1, 0, 0), (0, 1, 0), (0, 0, 1)))
 
     def test_ones_2_dimensional(self):
         """Test ones method on 2 by 2 matrix."""
-        self.assertEqual(Matrix.ones(2), Matrix(((1, 1), (1, 1))))
+        self.assertEqual(Matrix.ones(2).matrix_rows, ((1, 1), (1, 1)))
 
     def test_ones_3_dimensional(self):
         """Test ones method on 3 by 3 matrix."""
-        self.assertEqual(Matrix.ones(3),
-                         Matrix(((1, 1, 1), (1, 1, 1), (1, 1, 1))))
+        self.assertEqual(Matrix.ones(3).matrix_rows,
+                         ((1, 1, 1), (1, 1, 1), (1, 1, 1)))
 
     def test_multiplication_by_int(self):
         """Test multiplication by int."""
-        self.assertEqual(self.mat_a * 10, Matrix(((10, 20), (30, 40))))
+        self.assertEqual((self.mat_a * 10).matrix_rows, ((10, 20), (30, 40)))
 
     def test_multiplication_by_float(self):
         """Test multiplication by float."""
-        self.assertEqual(self.mat_a * 0.9, Matrix(((0.9, 1.8), (2.7, 3.6))))
+        self.assertEqual((self.mat_a * 0.9).matrix_rows, ((0.9, 1.8), (2.7, 3.6)))
 
     def test_multiplication_of_matrices(self):
         """Test multiplication of matrices."""
-        self.assertEqual(self.mat_a * self.mat_b, Matrix(((15, 21), (33, 47))))
+        self.assertEqual((self.mat_a * self.mat_b).matrix_rows, ((15, 21), (33, 47)))
 
-    def test_multiplication_reversed(self):
-        """Check reversed multiplication."""
-        self.assertEqual(10 * self.mat_a,
-                         Matrix(((10, 20), (30, 40))))
+    def test_multiply_scalar_reversed(self):
+        """Check reversed multiplication by scalar."""
+        self.assertEqual((10 * self.mat_a).matrix_rows,
+                         ((10, 20), (30, 40)))
+
+    def test_multiply_matrices_reversed(self):
+        """Check reversed multiplication of matrices."""
+        self.assertEqual((self.mat_b * self.mat_a).matrix_rows,
+                         ((18, 26), (30, 44)))
 
     def test_multiply_type_validation(self):
         """Test that you can only multiply numbers and matrices."""
@@ -73,11 +78,11 @@ class TestMatrix(unittest.TestCase):
 
     def test_division_by_int(self):
         """Test the method for division of matrix by int."""
-        self.assertEqual(self.mat_a / 2, Matrix(((0.5, 1.0), (1.5, 2.0))))
+        self.assertEqual((self.mat_a / 2).matrix_rows, ((0.5, 1.0), (1.5, 2.0)))
 
     def test_division_by_float(self):
         """Test the method for division of matrix by float."""
-        self.assertEqual(self.mat_a / 0.1, Matrix(((10, 20), (30, 40))))
+        self.assertEqual((self.mat_a / 0.1).matrix_rows, ((10, 20), (30, 40)))
 
     def test_division_Type_validation(self):
         """Test that division is only allowed with a number."""
@@ -86,8 +91,8 @@ class TestMatrix(unittest.TestCase):
 
     def test_addition(self):
         """Test the method for addition of 2 matrices."""
-        self.assertEqual(self.mat_a + Matrix.unity(2),
-                         Matrix(((2, 2), (3, 5))))
+        self.assertEqual((self.mat_a + self.mat_b).matrix_rows,
+                         ((4, 7), (9, 12)))
 
     def test_addition_type_validation(self):
         """Test that addition is only allowed between 2 matrices."""
@@ -101,7 +106,7 @@ class TestMatrix(unittest.TestCase):
 
     def test_subtraction(self):
         """Test the method for subtraction of 2 matrices."""
-        self.assertEqual(self.mat_a - self.mat_b, Matrix(((-2, -3), (-3, -4))))
+        self.assertEqual((self.mat_a - self.mat_b).matrix_rows, ((-2, -3), (-3, -4)))
 
     def test_subtraction_type_validation(self):
         """Test that subtraction is only allowed between 2 matrices."""
@@ -113,20 +118,29 @@ class TestMatrix(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.mat_a - Matrix.ones(3)
 
-    def test_iter(self):
-        """Test the iterator of the matrix."""
-        mat_a_iter = iter(self.mat_a)
-        self.assertEqual(next(mat_a_iter), (1, 2))
+    def test_iter_a(self):
+        """Test the iterator of the matrix a."""
+        self.assertEqual(list(self.mat_a), [(1, 2), (3, 4)])
 
-        self.assertEqual(next(mat_a_iter), (3, 4))
+    def test_iter_b(self):
+        """Test the iterator of the matrix b."""
+        self.assertEqual(list(self.mat_b), [(3, 5), (6, 8)])
 
-    def test_eq(self):
-        """Test the equality check between matrices."""
+    def test1_eq(self):
+        """Test the equality check between different matrices."""
         self.assertEqual(self.mat_a == self.mat_b, False)
 
-    def test_not_equal(self):
-        """Test if two matrices are not equal."""
+    def test2_eq(self):
+        """Test the equality check between equal matrices."""
+        self.assertEqual(self.mat_a == self.mat_a, True)
+
+    def test1_not_equal(self):
+        """Test if two different matrices are not equal."""
         self.assertEqual(self.mat_a != self.mat_b, True)
+
+    def test2_not_equal(self):
+        """Test if two equal matrices are not equal."""
+        self.assertEqual(self.mat_a != self.mat_a, False)
 
     def test_hash(self):
         """Test if hash method."""
