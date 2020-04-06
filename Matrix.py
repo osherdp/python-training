07dcc672cc5e95720d09fc4e37bfe057e745f66b
MichaelYarshansky <michaelyarshansky@gmail.com>
2020-04-06 16:13:28 +0300

    AFTER SECOND REVIEWE

--- Matrix.py
+++ Matrix.py
@@ -1,13 +1,23 @@
+""" in this program Matrix is a tuple of tuples and the internal tuples must be as long as the external list is.
+data is the Matrix (tuple) received from the user, and this class has add, sub, mul, scalar_mul and division by
+ scalar methods and non- mathemathic methods as repr, str, hash and eq."""
+
+
 class Matrix:
-    # in this program Matrix is a tuple of tuples and the inside tuples must be as long as the major ine is.
-    # data is the Matrix (tuple) received from the user
+    """A matrices calculator for basic math actions
+        Attributes:
+            data(tuple of tuples): a Matrix- each line is a tuple in the major tuple """
+
     def __init__(self, data):
-        counter = 0
+        #  inits a Matrix from received data (type= tuple), throws error for non- tuple inputs.
+        check_value = True
         if type(data) is tuple:
             for tup in data:
-                if len(tup) == len(data) and type(tup) is tuple:
-                    counter += 1
-        if counter == len(data):
+                if len(tup) != len(data) or type(tup) is not tuple:
+                    check_value *= 0
+        else:
+            check_value *= 0
+        if check_value:
             self.data = data
         else:
             raise ValueError("Invalid matrix")
@@ -19,7 +29,8 @@ class Matrix:
         return self.data[item]
 
     def __repr__(self):
-        return f"Matrix: {self.data}"
+        """prints the matrix in a format that can be used for build another Matrix"""
+        return f"Matrix({self.data})"
 
     # str returns the Matrix in the mathemathic format
     def __str__(self):
@@ -28,91 +39,128 @@ class Matrix:
             matrix_in_lines += (str(self.data[i]) + "\n")
         return matrix_in_lines
 
-    def get_tuple(self):
-        return self.data
-
     # unity method creates a matrix of zeros with a diagonal of ones (unity is the Matrix's size)
     @staticmethod
     def unity(unit):
-        major_list = [[1 if i == index else 0 for i in range(unit)] for index in range(unit)]
-        sub_list = [tuple(lt) for lt in major_list]  # convert list of lists to type Matrix. lt are the inside lists
+        sub_list = [tuple(lt) for lt in [[1 if i == index else 0 for i in range(unit)] for index in range(unit)]]
+        # converts list of lists to type Matrix. lt are the inside lists
         matrix_to_return = Matrix((tuple(sub_list)))
         return matrix_to_return
 
     # ones method returns a Matrix full of ones which sizes determined by "unit"
     @staticmethod
     def ones(unit):
-        major_list = [([1] * unit) for i in range(unit)]
-        sub_list = [tuple(lt) for lt in major_list]  # convert list of lists to type Matrix. lt are the inside lists
+        sub_list = [tuple(lt) for lt in [([1] * unit) for i in range(unit)]]
+        # convert list of lists to type Matrix. lt are the inside lists
         matrix_to_return = Matrix((tuple(sub_list)))
         return matrix_to_return
 
     def __add__(self, other):
+        """add a matrix to another one
+            args: self.data (tuple)- A matrix
+                other(tuple) -A matrix
+
+            returns: return the new matrix (tuple)
+
+             raises: syntaxerror if "other" isn't a matrix"""
         if type(other) is Matrix:
-            ans = []
+            answer = []
             for tup in range(len(self.data)):
-                ans.append([])
+                answer.append([])
                 for num in range(len(self.data)):
-                    ans[tup].append(self.data[tup][num] + other[tup][num])
-            sub_list = [tuple(ls) for ls in ans]  # convert ans to type Matrix. ls are the inside lists
-            matrix_to_return = Matrix((tuple(sub_list)))
-            return matrix_to_return
+                    answer[tup].append(self.data[tup][num] + other[tup][num])
+            sub_list = [tuple(ls) for ls in answer]  # convert answer to type Matrix. ls are the inside lists
+            return Matrix((tuple(sub_list)))
         else:
-            raise SyntaxError("only matrix's summery")
+            raise SyntaxError("only matrix's can be added together")
 
     def __sub__(self, other):
+        """subtracting a matrix from another
+                    args: self.data (tuple)- A matrix
+                        other(tuple) -A matrix
+
+                    returns: return the new matrix (tuple)
+
+                     raises: syntaxerror if "other" isn't a matrix"""
         if type(other) is Matrix:
-            ans = []
+            answer = []
             for tup in range(len(self.data)):
-                ans.append([])
+                answer.append([])
                 for num in range(len(self.data)):
-                    ans[tup].append(self.data[tup][num] - other[tup][num])
-            sub_list = [tuple(ls) for ls in ans]  # convert list of lists to type Matrix. ls are the inside lists
-            matrix_to_return = Matrix((tuple(sub_list)))
-            return matrix_to_return
+                    answer[tup].append(self.data[tup][num] - other[tup][num])
+            sub_list = [tuple(ls) for ls in answer]  # convert list of lists to type Matrix. ls are the inside lists
+            return Matrix((tuple(sub_list)))
         else:
             raise SyntaxError("only matrix's subscription")
 
     def __truediv__(self, other):
+        """divides a matrix by scalar
+                    args: self.data (tuple)- A matrix
+                        other(int/float) -A scalar
+
+                    returns: return the new matrix (tuple)
+
+                     raises: syntaxerror if "other" isn't a scalar"""
         if type(other) is float or int:  # the program doesnt support division by other Matrix
-            return self.scalar_mul(1/other)
+            return self.scalar_mul(1 / other)
         else:
             raise SyntaxError("only  scalar division")
 
     def __mul__(self, other):
+        if type(other) is Matrix:
+            return self.matrices_mul(other)
+        elif type(other) is float or type(other) is int:
+            return self.scalar_mul(other)
+        else:
+            raise SyntaxError("invalid input")
+
+    def matrices_mul(self, other):
+        """multiply a matrix by matrix
+                    args: self.data (tuple)- A matrix
+                        other(tuple) -A matrix
+
+                    returns: return the new matrix (tuple)"""
         if type(other) is Matrix:  # Matrix's multiplication --->
-            ans = []  # will contain the answer as a list of lists.
+            answer = []  # will contain the answer as a list of lists.
             new_other = []
             # this loop convert "other" from Matrix type to a list of lists named "new_other".
             for line in range(len(self.data)):
                 new_other.append([])
-                ans.append([0] * len(self.data))  # ns starts as a list of lists full of zeros.
+                answer.append([0] * len(self.data))  # ns starts as a list of lists full of zeros.
                 for num in range(len(self.data)):
                     new_other[line].append(other[num][line])
             # this is the multiplication process->
             for tup in range(len(self.data)):
                 for num in range(len(self.data)):
                     for i in range(len(self.data)):
-                        ans[tup][num] += self.data[tup][i] * new_other[num][i]
-            # here i added to the zero the summery of the multiplications of any two numbers which
+                        answer[tup][num] += self.data[tup][i] * new_other[num][i]
+            # here the program adds to the zero the summery of the multiplications of any two numbers which
             # their places in the matrices are fits by the matrices multiplication rules.
-            sub_list = [tuple(ls) for ls in ans]  # convert ans to type Matrix. ls are the inside lists.
-            matrix_to_return = Matrix((tuple(sub_list)))
-            return matrix_to_return
-        else:
-            return self.scalar_mul(other)
+            sub_list = [tuple(ls) for ls in answer]  # convert answer to type Matrix. ls are the inside lists.
+            return Matrix((tuple(sub_list)))
 
     def scalar_mul(self, other):
-        ans = []
+        """multiply a matrix by a scalar
+                    args: self.data (tuple)- A matrix
+                        other(int/float) -A scalar
+
+                    returns: return the new matrix (tuple)
+
+                     raises: Syntaxerror if "other" isn't a scalar"""
+        answer = []
         for tup in range(len(self.data)):
-            ans.append([])
+            answer.append([])
             for num in range(len(self.data)):
-                ans[tup].append(self.data[tup][num] * other)
-        sub_list = [tuple(ls) for ls in ans]  # convert ans to type Matrix. ls are the inside lists
-        matrix_to_return = Matrix((tuple(sub_list)))
-        return matrix_to_return
+                answer[tup].append(self.data[tup][num] * other)
+        sub_list = [tuple(ls) for ls in answer]  # convert ans to type Matrix. ls are the inside lists
+        return Matrix((tuple(sub_list)))
 
     def __eq__(self, other):
+        """checks if two matrices are equal
+                    args: self.data (tuple)- A matrix
+                        other- anything
+
+                    returns: True/False """
         return self.data == other
 
     # let matrix instances to be a key in a dictionary.
--- test_matrix.py
+++ test_matrix.py
@@ -12,39 +12,56 @@ class Test_matrix(unittest.TestCase):
         self.three = Matrix(((1, 2, 0), (0, 1, 2), (2, 0, 1)))
         self.half = Matrix(((0.5, 0.5), (0.5, 0.5)))
 
+    def test_init_failed(self):
+        with self.assertRaises(ValueError):
+            (Matrix("hello"))
+        with self.assertRaises(ValueError):
+            (Matrix(((2, 1), (1, 2, 3))))
+
     def test_add(self):
-        z = Matrix(((5, 5), (5, 5)))
-        self.assertEqual(self.x + self.y, z)
-        t = Matrix(((-1, -1), (-1, -1)))
-        f = Matrix(((-2, -3), (-4, -5)))
-        self.assertEqual(self.x + f, t)
+        self.assertEqual(self.x + self.y, Matrix(((5, 5), (5, 5))))
+        self.assertEqual(self.x + Matrix(((-2, -3), (-4, -5))), Matrix(((-1, -1), (-1, -1))))
         self.assertEqual(self.three + self.three, Matrix(((2, 4, 0), (0, 2, 4), (4, 0, 2))))
+
+    def test_add_failed(self):
+        with self.assertRaises(SyntaxError):
+            (Matrix.__add__(self.x, 2))
 
     def test_mul(self):
         self.assertEqual(self.zeros * self.x, self.zeros)
         s = Matrix(((1, 1, 1), (1, 1, 1), (1, 1, 1)))
         self.assertEqual(s * self.three, Matrix(((3, 3, 3), (3, 3, 3), (3, 3, 3))))
-        self.assertEqual(self.x * self.y, Matrix(((8, 5), (20, 13))))
         self.assertEqual(self.tens * self.half, self.tens)
         self.assertEqual(self.negative * self.negative, Matrix(((2, 2), (2, 2))))
-
-    def test_scalar_mul(self):
         self.assertEqual(self.x * 3, Matrix(((3, 6), (9, 12))))
         self.assertEqual(self.x * 0, Matrix(((0, 0), (0, 0))))
         self.assertEqual(self.x * -0.5, Matrix(((-0.5, -1), (-1.5, -2))))
 
+    def test_mul_failed(self):
+        with self.assertRaises(SyntaxError):
+            (Matrix.__mul__(self.x, "l"))
+
     def test_sub(self):
-        z = Matrix(((-3, -1), (1, 3)))
-        self.assertEqual(self.x - self.y, z)
-        t = Matrix(((3, 5), (7, 9)))
-        f = Matrix(((-2, -3), (-4, -5)))
-        self.assertEqual(self.x - f, t)
+        self.assertEqual(self.x - self.y, Matrix(((-3, -1), (1, 3))))
+        self.assertEqual(self.x - Matrix(((-2, -3), (-4, -5))), Matrix(((3, 5), (7, 9))))
         self.assertEqual(self.three - self.three, Matrix(((0, 0, 0), (0, 0, 0), (0, 0, 0))))
+
+    def test_sub_failed(self):
+        with self.assertRaises(SyntaxError):
+            (Matrix.__sub__(self.x, 2))
 
     def test_div(self):
         self.assertEqual(self.x / 2, Matrix(((0.5, 1), (1.5, 2))))
         self.assertEqual(self.x / 0.5, Matrix(((2, 4), (6, 8))))
         self.assertEqual(self.x / -1, Matrix(((-1, -2), (-3, -4))))
+
+    def test_div_failed(self):
+        with self.assertRaises(TypeError):
+            (Matrix.__truediv__(self.x, self.x))
+
+    def zero_division(self):
+        with self.assertRaises(ZeroDivisionError):
+            (Matrix.__truediv__(self.x, 0))
 
     def test_unity(self):
         self.assertEqual(Matrix.unity(3), Matrix(((1, 0, 0), (0, 1, 0), (0, 0, 1))))
