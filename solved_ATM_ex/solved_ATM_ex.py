e027613ce177bb7e42c0b5494de42920fc06305c
lielya <liel.nana1@gmail.com>
2020-04-08 14:57:54 +0300

    resolve ATM exercise with json file (nested dictionary)

--- solved_ATM_ex/solved_ATM_ex.py
+++ solved_ATM_ex/solved_ATM_ex.py
@@ -1,165 +1,140 @@
 """Simulating an ATM. """
 
 import sys
+import json
 
 ATM_FILE = 1  # The number 1 refers to the place of the file as a parameter.
-COSTUMER_BALANCE_PLACE_IN_DICTIONARY = 1
-COSTUMER_PASSWORD_PLACE_IN_DICTIONARY = 0
 
 
-def cash_withdrawal(costumer_indent, amount_to_withdrawal,
-                    costumers_dictionary):
+def cash_withdrawal(costumer_id, costumers_data):
     """Withdrawal from the ATM amount of money.
 
     Args:
-        costumer_indent (string): Costumer id.
-        amount_to_withdrawal (float): Amount the costumer wants to withdrawal.
-        costumers_dictionary (dictionary): Contains bank costumer data.
+        costumer_id (string): Costumer id.
+        costumers_data (dictionary): Bank costumer data.
 
     Returns:
-        True for success withdrawal action, False otherwise.
-
+        boolean: True for success withdrawal action, False otherwise.
     """
-    balance_now = float(costumers_dictionary[costumer_indent][
-                            COSTUMER_BALANCE_PLACE_IN_DICTIONARY])
-
+    amount_to_withdrawal = float(input("Amount to withdrawal: "))
+    balance_now = float(costumers_data[costumer_id]['balance'])
     if 0 < amount_to_withdrawal <= balance_now:
         new_balance = balance_now - amount_to_withdrawal
-        costumers_dictionary[costumer_indent][
-            COSTUMER_BALANCE_PLACE_IN_DICTIONARY] = str(new_balance)
+        costumers_data[costumer_id]['balance'] = new_balance
         return True
 
     return False
 
 
-def cash_deposit(costumer_indent, amount_to_deposit, costumers_dictionary):
-    """Deposit to the bank some of money.
+def cash_deposit(costumer_id, costumers_data):
+    """Deposit money to the bank.
 
     Args:
-        costumer_indent (string): Costumer id.
-        amount_to_deposit (float): Amount the costumer wants to deposit.
-        costumers_dictionary (dictionary): Contains bank costumer data.
+        costumer_id (string): Costumer id.
+        costumers_data (dictionary): Bank costumer data.
 
     Returns:
-        True for success deposit action, False otherwise.
-
+        boolean: True for success deposit action, False otherwise.
     """
+    amount_to_deposit = float(input("Amount to deposit: "))
     if amount_to_deposit > 0:
-        balance_now = float(costumers_dictionary[costumer_indent][
-                                COSTUMER_BALANCE_PLACE_IN_DICTIONARY])
+        balance_now = float(costumers_data[costumer_id]['balance'])
         new_balance = balance_now + amount_to_deposit
-        costumers_dictionary[costumer_indent][
-            COSTUMER_BALANCE_PLACE_IN_DICTIONARY] = str(new_balance)
+        costumers_data[costumer_id]['balance'] = new_balance
         return True
 
     return False
 
 
-def change_password(costumer_indent, new_password, costumers_dictionary):
-    """Changing the costumer's ATM password.
+def change_password(costumer_id, costumers_data, new_password):
+    """Changing costumer's ATM password.
 
     Args:
-        costumer_indent (string): Costumer id.
-        new_password (string): The new password the costumer wants to
-                               change to.
-        costumers_dictionary (dictionary): Contains bank costumer data.
+        costumer_id (string): Costumer id.
+        costumers_data (dictionary): Bank costumer data.
+        new_password (string): Password the costumer wants to change to.
 
     Returns:
-        True for success changing the password, False otherwise.
-
+        boolean: True for success changing the password, False otherwise.
     """
     if len(new_password) == 4 and new_password.isdigit():
-        costumers_dictionary[costumer_indent][
-            COSTUMER_PASSWORD_PLACE_IN_DICTIONARY] = new_password
-        return True
+        costumers_data[costumer_id]['password'] = new_password
+        return new_password
 
     return False
 
 
-def fill_dictionary(costumers_dictionary):
-    """Filling dictionary with bank customer data.
+def create_dictionary(path_of_file):
+    """Create dictionary of bank customer data.
 
     Args:
-         costumers_dictionary (dictionary): An empty dictionary.
+        path_of_file (string): Path of the file that contains all bank
+        costumer data.
 
     Returns:
-        A full dictionary of bank costumer data, or an exception if the
-        file path is not correct.
-
+        dictionary: A nested dictionary of bank costumer data, or an exception
+        if the file path is not correct.
     """
     try:
-        with open(sys.argv[ATM_FILE], 'r') as atm_file:
-            for line in atm_file:
-                a, *b = line.split()
-                costumers_dictionary[a] = b
-            return costumers_dictionary
+        with open(path_of_file, 'r') as file_j:
+            data = json.load(file_j)
+            costumers_data = data
+            return costumers_data
 
     except FileNotFoundError:
         return "Your file path is incorrect. Please check it."
 
 
 def main():
-    costumers_dictionary = {}
-    fill_dictionary(costumers_dictionary)
-    print("Welcome to bank!\n")
-    costumer_indent = input("Enter your id: ")
-    costumer_password = input("Enter your password: ")
-    not_finish_actions = True
+    costumers_data = create_dictionary(path_of_file=sys.argv[ATM_FILE])
+    costumer_id = input("Welcome to the bank\n\nEnter your id: \n")
+    costumer_password = input("Enter your password: \n")
 
-    while not_finish_actions:
-        if costumer_indent in costumers_dictionary and \
-                costumers_dictionary[costumer_indent][
-                    COSTUMER_PASSWORD_PLACE_IN_DICTIONARY] \
-                == costumer_password:
+    while costumer_id != '-1':
+        if costumer_id in costumers_data and costumers_data[costumer_id] \
+                ['password'] == costumer_password:
 
             try:
-                print("For checking your balance press 1")
-                print("For cash withdrawal press 2")
-                print("For cash deposit press 3")
-                print("For change password press 4")
-                print("To exit press -1")
-                costumer_choice = int(input("Pressing: \n"))
+                costumer_choice = int(input("For checking your balance press"
+                        " 1 \nFor cash withdrawal press 2 \nFor cash deposit"
+                        " press 3 \nFor change password press 4 \nTo finish "
+                        "actions press 5 \n"))
 
                 if costumer_choice == 1:
-                    print(f"Your balance: "
-                          f"{float(costumers_dictionary[costumer_indent][COSTUMER_BALANCE_PLACE_IN_DICTIONARY])}\n")
+                    print(
+                        f"Balance: {costumers_data[costumer_id]['balance']}")
 
                 elif costumer_choice == 2:
-                    amount_to_withdrawal = float(input("Please enter amount "
-                                               "you want to withdrawal: \n"))
-
-                    if cash_withdrawal(costumer_indent, amount_to_withdrawal,
-                                       costumers_dictionary):
+                    if cash_withdrawal(costumer_id, costumers_data):
                         print("The action was succeeded\n")
 
                     else:
                         print("Failed - check your amount\n")
 
                 elif costumer_choice == 3:
-                    amount_to_deposit = float(input("Please enter amount you "
-                                                    "want to deposit: \n"))
-
-                    if cash_deposit(costumer_indent, amount_to_deposit,
-                                    costumers_dictionary):
+                    if cash_deposit(costumer_id, costumers_data):
                         print("The action was succeeded\n")
 
                     else:
                         print("Failed - check your amount\n")
 
                 elif costumer_choice == 4:
-                    new_password = input("Enter a new password: \n")
-                    if change_password(costumer_indent, new_password,
-                                       costumers_dictionary):
+                    new_password = input("Enter a new password: ")
+                    if change_password(costumer_id, costumers_data,
+                                       new_password):
+                        costumer_password = new_password
                         print("Password changed")
 
                     else:
-                        print("Not a good password, needs to be 4 digits:")
+                        print("Not a good password,it needs to be 4 digits.")
 
-                elif costumer_choice == -1:
-                    not_finish_actions = False
+                elif costumer_choice == 5:
+                    costumer_id = input("Enter your id: ")
+                    if costumer_id != '-1':
+                        costumer_password = input("Enter your password: ")
 
                 else:
-                    print("Choose 1/2/3/4/-1 option: ")
+                    print("Choose 1/2/3/4/5 option: ")
 
             except ValueError:
                 print("Value Error! your input needs to be digit value\n")
@@ -167,13 +142,12 @@ def main():
         else:
             print("Your ID or your password is not correct. Please try to "
                   "enter again:\n")
-            costumer_indent = input("Enter your id: ")
+            costumer_id = input("Enter your id: ")
             costumer_password = input("Enter your password: ")
 
-        with open(sys.argv[ATM_FILE], 'w') as atm_file:
-            for line in costumers_dictionary:
-                atm_file.write(
-                    f"{line} {' '.join(costumers_dictionary[line])}\n")
+    data = json.dumps(costumers_data)
+    with open(sys.argv[ATM_FILE], 'w') as atm_file:
+        atm_file.write(data)
 
 
 if __name__ == '__main__':
