28a46fe9272490ce282c24eab0c31308c107e370
solm11 <solben.itay@gmail.com>
2020-04-13 11:09:30 +0200

    author removed, one line doc at the start of the file that explains the purpose of the file, reordered the imports, imports and consts separated, more meaningful names to the consts, removed redundant .

--- Exercises/STAM_Exercise.py
+++ Exercises/STAM_Exercise.py
@@ -1,37 +1,29 @@
-__author__ = "Sol"
+# The purpose of this file is to answer the STAM exercise.
 
-
-import sys
 import os
 import shutil
-FILE = 1
-PATH = 2
+import sys
+
+
+FILE_INDEX = 1
+PATH_INDEX = 2
 
 
 def main():
-    text_file = sys.argv[FILE]
-    path = sys.argv[PATH]
-    is_exist(text_file, path)
-    add_strings(text_file, path)
+    original_text_file = sys.argv[FILE_INDEX]
+    path_of_new_file = sys.argv[PATH_INDEX]
+    if is_exist(original_text_file, path_of_new_file):
+        add_strings(original_text_file, path_of_new_file)
 
 
 def is_exist(text_file, path):
     """
-    checks if the text file is a file
-    checks if the path exist
-    print the appropriate response
+    checks if the text file and the path exist
     :param text_file: input text file
     :param path: input path
     :return: True/ False
     """
-    if os.path.isfile(text_file):
-        if os.path.exists(path):
-            # if both exist and OK returns True
-            return True
-        print("The path not exist")
-        return False
-    print("This is not a text file")
-    return False
+    return text_file and path
 
 
 def add_strings(text_file, path):
@@ -47,7 +39,7 @@ def add_strings(text_file, path):
     with open(text_file, 'r') as text_file:
         with open("new_file.txt", 'w+') as new_file:
             for line in text_file:
-                text_of_line = line[0: len(line) - 1]  # remove the '\n'
+                text_of_line = line[:-1]  # remove the '\n'
                 new_file.write("Sol {} Mirilashvili ".format(text_of_line) + '\n')
     try:
         shutil.move("new_file.txt", path)
