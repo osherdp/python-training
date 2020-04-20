8a27fe33535362c51158dc121c21686bc015df43
Yonatan Evantal <yonatan.evantal@gmail.com>
2020-04-20 12:18:18 +0300

    First working stable version

--- calculator/calculator_sandbox.py
+++ calculator/calculator_sandbox.py
@@ -8,37 +8,62 @@ def remove_spaces(expression: str) -> str:
     return ''.join(expression.split())
 
 
+def check_operands(func):
+    """Check the existence of the operands.
+
+    Args:
+        func (function): the function to operate on.
+
+    Returns:
+        function: the function wrapper.
+    """
+
+    def operands_wrapper(operator_symbol, operands):
+        left_operand, right_operand = operands
+
+        if left_operand and right_operand:
+            return func(operator_symbol, operands)
+
+        elif not left_operand and right_operand:
+            if any([operator_symbol == "+", operator_symbol == "-"]):
+                return float(f"{operator_symbol}{right_operand}")
+
+            return func(operator_symbol, [right_operand])
+
+        elif not right_operand and left_operand:
+            return func(operator_symbol, [left_operand])
+
+    return operands_wrapper
+
+
 def get_inner_parentheses(expression: str) -> str:
     """Return the most inner parentheses in an expression."""
     pattern = re.compile(r"\([^()]*\)")
     without_outer_parentheses = [inner_expr[1:-1] for inner_expr in
                                  pattern.findall(expression)]
+
     if without_outer_parentheses:
         return without_outer_parentheses[-1]
 
     return expression
 
 
-def calculate_sub_expression(operator_symbol, left_operand, right_operand):
+@check_operands
+def calculate_sub_expression(operator_symbol, operands):
     """Evaluate an expression.
 
     Args:
-        left_operand (str): the left operand.
-        right_operand (str): the right operand.
+        operands (list): the operands, can be 2 or 1 according to operation.
         operator_symbol (str): a symbol represents the mathematical operation.
 
     Returns:
         A float, the result of the mathematical operation.
     """
-    if left_operand and right_operand:
-        return operators[operator_symbol].operation(float(left_operand),
-                                                    float(right_operand))
+    if len(operands) == 2:
+        return operators[operator_symbol].operation(float(operands[0]),
+                                                    float(operands[1]))
 
-    elif not left_operand and right_operand:
-        return operators[operator_symbol].operation(float(right_operand))
-
-    elif not right_operand and left_operand:
-        return operators[operator_symbol].operation(float(left_operand))
+    return operators[operator_symbol].operation(float(operands[0]))
 
 
 def get_highest_precedence_operator(expression: str) -> str:
@@ -69,6 +94,8 @@ def parse_sub_expression_by_operator(sub_expression, operator):
                          rf"?P<right_operand>{any_int_or_float})?")
 
     matches = pattern.search(sub_expression).groupdict()
+    # replace None with ""
+    matches = {k: ("" if not v else v) for k, v in matches.items()}
 
     return matches["left_operand"], matches["right_operand"]
 
@@ -126,50 +153,24 @@ def replace_in_expression(old_expr, old_sub_expr, new_sub_expr):
     return old_expr.replace(f"{old_sub_expr}", new_sub_expr)
 
 
-def check_operands(func):
-    def wrapper(operator_symbol, left_operand, right_operand):
-        if left_operand and right_operand:
-            func(operator_symbol, )
-
-        elif not left_operand and right_operand:
-            func()
-
-        elif not right_operand and left_operand:
-            func()
-
-    return wrapper
-
-
 def evaluate(expression: str) -> str:
     """Calculates the value of a mathematical expression."""
-    print(expression)
-    sub_expression = get_inner_parentheses(expression)
+    if is_number(expression):
+        return expression
 
-    if not has_operations(expression):
-        return sub_expression
+    sub_expression = get_inner_parentheses(expression)
 
     left_operand, right_operand, current_operator = parse_sub_expression(
         sub_expression)
 
-    current_result = calculate_sub_expression(current_operator, left_operand,
-                                              right_operand)
+    current_result = calculate_sub_expression(current_operator,
+                                              [left_operand, right_operand])
 
     updated_sub_expression = sub_expression.replace(
         f"{left_operand}{current_operator}{right_operand}",
         str(current_result))
 
-    print(updated_sub_expression)
-
     expression = replace_in_expression(expression, sub_expression,
                                        updated_sub_expression)
 
     return evaluate(expression)
-
-
-if __name__ == '__main__':
-    # source = '(((81 * 6) /42 $ (5*(3.5 @ 7)-1)))'
-    # source = "(100^0.5)"
-    # source = "(1 + (3*(3^0.5)))"
-    source = "(3! + 3^(2!))"
-
-    print(evaluate(remove_spaces(source)))
