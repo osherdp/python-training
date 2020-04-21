609ac34f13badeaf9ef8f859409858320a690bc3
lielya <liel.nana1@gmail.com>
2020-04-21 14:40:13 +0300

    switch case global variables

--- solved_ATM_ex/solved_ATM_ex.py
+++ solved_ATM_ex/solved_ATM_ex.py
@@ -1,25 +1,23 @@
-"""Simulating an ATM. """
-
 import os
 import sys
 import json
 
 ATM_FILE_PLACE = 1
-COSTUMER_CHOICE_OPTIONS = "For checking balance press  1 \nFor cash " \
-                          "withdrawal press 2 \nFor cash deposit press 3 " \
-                          "\nFor change password " \
-                          "press 4 \nTo finis actions press 5 \n"
+COSTUMER_CHOICE_OPTIONS = "1 - View balance \n2 - Withdraw \n3 - Deposit " \
+                          "\n4 - Change password \n5 - Exit\n"
+
+costumer_id = ""
+costumer_password = ""
 
 
-def cash_withdrawal(costumer_id, costumers_data):
-    """Withdrawal from the ATM amount of money.
+def cash_withdraw(costumers_data):
+    """Withdraw amount of money from the ATM .
 
     Args:
-        costumer_id (string): Costumer id.
         costumers_data (dictionary): Bank costumer data.
 
     Returns:
-        boolean: True for success withdrawal action, False otherwise.
+        boolean: True for success withdraw action, False otherwise.
     """
     amount_to_withdrawal = float(input("Amount to withdrawal: "))
     balance_now = float(costumers_data[costumer_id]['balance'])
@@ -28,14 +26,14 @@ def cash_withdrawal(costumer_id, costumers_data):
         costumers_data[costumer_id]['balance'] = new_balance
         return True
 
+    print("Failed - check amount\n")
     return False
 
 
-def cash_deposit(costumer_id, costumers_data):
+def cash_deposit(costumers_data):
     """Deposit money to the bank.
 
     Args:
-        costumer_id (string): Costumer id.
         costumers_data (dictionary): Bank costumer data.
 
     Returns:
@@ -48,25 +46,27 @@ def cash_deposit(costumer_id, costumers_data):
         costumers_data[costumer_id]['balance'] = new_balance
         return True
 
+    print("Failed - check amount\n")
     return False
 
 
-def change_password(costumer_id, costumers_data, new_password):
-    """Changing costumer's ATM password.
+def change_password(costumers_data):
+    """Change costumer's password.
 
     Args:
-        costumer_id (string): Costumer id.
         costumers_data (dictionary): Bank costumer data.
-        new_password (string): Password the costumer wants to change to.
 
     Returns:
         boolean: True for success changing the password, False otherwise.
     """
+    global costumer_password
+    new_password = input("New password: ")
     if len(new_password) == 4 and new_password.isdigit():
         costumers_data[costumer_id]['password'] = new_password
-        password_now = new_password
-        return password_now
+        costumer_password = new_password
+        return True
 
+    print("Should be 4 digits.")
     return False
 
 
@@ -78,8 +78,7 @@ def create_dictionary(path_of_file):
             data.
 
     Returns:
-        dictionary: A nested dictionary of bank costumer data, or print a
-            message and return a None dict if the file path is not correct.
+        dictionary: A nested dictionary of bank costumer data, or None.
     """
     if os.path.isfile(path_of_file):
         with open(path_of_file, 'r') as file_j:
@@ -88,99 +87,67 @@ def create_dictionary(path_of_file):
             return costumers_data
 
     print("File not found")
-    costumers_data = None
-    return costumers_data
+    return None
 
 
-def check_costumer_choice(costumer_choice, costumers_data, costumer_id,
-                          costumer_password):
-    """Check costumer choice and act accordingly.
+def get_new_costumer():
+    """Get a new costumer. """
+
+    global costumer_id, costumer_password
+    costumer_id = input("id: ")
+    if costumer_id != '-1':
+        costumer_password = input("Password: ")
+
+
+def check_costumer_choice(costumer_choice, costumers_data):
+    """Check costumer's choice and act accordingly.
 
     Args:
         costumer_choice (integer): Costumer choice from 1/2/3/4/5 options,
             references to the action he wants to do in ATM.
         costumers_data (dictionary): Bank costumer data.
-        costumer_id (string): Costumer id.
-        costumer_password (string): Current costumer password.
 
     Returns:
-        string: Updated password if the choice is 4, or updated id and
-            password if the choice is 5.
+        string: An error message if costumer's choice is not in range.
     """
-    if costumer_choice == 1:
-        print(f"Balance: {costumers_data[costumer_id]['balance']}")
+    switcher = {
+        1: lambda: print(costumers_data[costumer_id]['balance']),
+        2: lambda: cash_withdraw(costumers_data),
+        3: lambda: cash_deposit(costumers_data),
+        4: lambda: change_password(costumers_data),
+        5: lambda: main()
+    }
 
-    elif costumer_choice == 2:
-        print("Action succeeded\n" if cash_withdrawal(
-            costumer_id, costumers_data) else "Failed - check amount\n")
-
-    elif costumer_choice == 3:
-        print(
-            "Action succeeded\n" if cash_deposit(costumer_id, costumers_data)
-            else "Failed - check amount\n")
-
-    elif costumer_choice == 4:
-        new_password = input("New password: ")
-        costumer_password = new_password if change_password(
-            costumer_id, costumers_data, new_password) else costumer_password
-        return costumer_password
-
-    elif costumer_choice == 5:
-        costumer_id = input("id: ")
-        if costumer_id != '-1':
-            costumer_password = input("Password: ")
-        return costumer_id, costumer_password
-
-    else:
-        print("Please choose 1/2/3/4/5 option: ")
+    return switcher.get(costumer_choice, lambda: print("Invalid option"))()
 
 
-def manage_costumer_actions(costumer_id, costumers_data, costumer_password):
-    """Manage costumer actions.
+def manage_costumer_actions(costumers_data):
+    """Manages costumer actions.
 
      Args:
         costumers_data (dictionary): Bank costumer data.
-        costumer_id (string): Costumer id.
-        costumer_password (string): Current costumer password.
-
-    Returns:
-        None.
     """
+    global costumer_password, costumer_id
     while costumer_id != '-1':
         if costumer_id in costumers_data and costumers_data[
                 costumer_id]['password'] == costumer_password:
             try:
                 costumer_choice = int(input(COSTUMER_CHOICE_OPTIONS))
-                if costumer_choice == 4:
-                    costumer_password = check_costumer_choice(costumer_choice,
-                                                              costumers_data,
-                                                              costumer_id,
-                                                              costumer_password)
-
-                elif costumer_choice == 5:
-                    costumer_id, costumer_password = check_costumer_choice(
-                        costumer_choice, costumers_data, costumer_id,
-                        costumer_password)
-
-                else:
-                    check_costumer_choice(costumer_choice, costumers_data,
-                                          costumer_id, costumer_password)
+                check_costumer_choice(costumer_choice, costumers_data)
 
             except ValueError:
                 print("Value Error! your input needs to be digit value\n")
 
         else:
-            print("ID or password is not correct.Try to enter again:\n")
-            costumer_id, costumer_password = input("id: "), input(
-                "Password: ")
+            print("id or password is not correct. Try again:\n")
+            get_new_costumer()
 
 
 def main():
+    get_new_costumer()
     costumers_data = create_dictionary(path_of_file=sys.argv[ATM_FILE_PLACE])
     if costumers_data is not None:
-        costumer_id, costumer_password = input("id: "), input("Password: ")
-        manage_costumer_actions(costumer_id, costumers_data,
-                                costumer_password)
+        manage_costumer_actions(costumers_data)
 
         with open(sys.argv[ATM_FILE_PLACE], 'w') as atm_file:
             atm_file.write(json.dumps(costumers_data))
